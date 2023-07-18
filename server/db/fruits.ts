9f6d88c5ad8d51fa6ffca15cc02f24e336dd97e6
Daniel Grabczewski <104661123+daniel-grabczewski@users.noreply.github.com>
2023-07-19 10:21:30 +1200

    Delete fruits.ts

--- server/db/fruits.ts
+++
@@ -1,6 +0,0 @@
-import connection from './connection'
-import { Fruit } from '../../models/fruit'
-
-export function getAllFruits(db = connection): Promise<Fruit[]> {
-  return db('fruit').select()
-}
