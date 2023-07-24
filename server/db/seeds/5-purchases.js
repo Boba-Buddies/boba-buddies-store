91db4de653cc189712f375a026b1e6e8fe19d02a
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-07-25 10:52:50 +1200

    complete purchases seed

--- server/db/seeds/5-purchases.js
+++ server/db/seeds/5-purchases.js
@@ -1,13 +1,37 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
- */
-exports.seed = async function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
-};
+  return knex('purchases')
+    .del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('purchases').insert([
+        {
+          id: 1,
+          user_id: 'auth0|abc12345',
+          product_id: 9,
+          quantity: 1,
+          purchased_at: '2023-02-10 10:30:00',
+          shipping_id: 1,
+          order_id: 1,
+        },
+        {
+          id: 2,
+          user_id: 'auth0|abc12345',
+          product_id: 10,
+          quantity: 6,
+          purchased_at: '2023-02-10 10:30:00',
+          shipping_id: 1,
+          order_id: 1,
+        },
+        {
+          id: 3,
+          user_id: 'auth0|abc12345',
+          product_id: 14,
+          quantity: 5,
+          purchased_at: '2023-02-10 10:30:00',
+          shipping_id: 1,
+          order_id: 1,
+        },
+      ])
+    })
+}
