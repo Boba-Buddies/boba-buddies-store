5c631fe0664b5dabda479a38abef7d75b6dd4ad0
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-07-25 08:46:08 +1200

    complete migration for users table

--- server/db/migrations/20230724202811_users.js
+++ server/db/migrations/20230724202811_users.js
@@ -2,14 +2,27 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
+exports.up = function (knex) {
+  return knex.schema.createTable('users', function (table) {
+    table.string('auth0_id').primary()
+    table.string('first_name').notNullable()
+    table.string('last_name').notNullable()
+    table.string('user_name').notNullable()
+    table.string('phone_number').notNullable()
+    table.string('address').notNullable()
+    table.string('city').notNullable()
+    table.string('country').notNullable()
+    table.string('zip_code').notNullable()
+    table.string('email_address').notNullable()
+    table.timestamp('created_at').notNullable()
+    table.boolean('is_admin').defaultTo(false)
+  })
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('users')
+}
