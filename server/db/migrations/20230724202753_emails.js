d40030621aa72a41bcf5298e038133f35a735982
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-07-25 08:37:26 +1200

    complete migration for emails table

--- server/db/migrations/20230724202753_emails.js
+++ server/db/migrations/20230724202753_emails.js
@@ -3,7 +3,14 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('emails', function(table) {
+    table.increments('id').primary();
+    table.string('user_id').notNullable();
+    table.boolean('is_read').defaultTo(false);
+    table.string('title').notNullable();
+    table.string('description').notNullable();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+  });
 };
 
 /**
@@ -11,5 +18,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('emails');
 };
