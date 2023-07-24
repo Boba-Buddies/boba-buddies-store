d9fdda725afe01489eeb19854ba0c3aad4b2ba12
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-07-25 08:48:13 +1200

    compelte migration for shipping_options table

--- server/db/migrations/20230724202824_shipping_options.js
+++ server/db/migrations/20230724202824_shipping_options.js
@@ -2,14 +2,18 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
+exports.up = function (knex) {
+  return knex.schema.createTable('shipping_options', function (table) {
+    table.increments('id').primary()
+    table.string('shipping_type').notNullable()
+    table.decimal('price').notNullable()
+  })
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('shipping_options')
+}
