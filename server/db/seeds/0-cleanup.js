d072741bccad4ac96f922354c885cd9dcae6b25c
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-07-25 10:48:36 +1200

    delete unneeded seeds

--- server/db/seeds/0-cleanup.js
+++ server/db/seeds/0-cleanup.js
@@ -1,15 +1,13 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
+exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('wishlist').del()
   await knex('reviews').del()
   await knex('purchases').del()
-  await knex('cart').del()
   await knex('emails').del()
   await knex('users').del()
   await knex('shipping_options').del()
   await knex('products').del()
-};
+}
