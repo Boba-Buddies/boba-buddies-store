1a6f562c1f4370acf6f2aa94221f4cf451cafebd
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-07-26 11:08:35 +1200

    create getAmountOfReviewsByDate

--- server/db/reviews.ts
+++ server/db/reviews.ts
@@ -17,8 +17,7 @@ PATCH: updateReviewStatusById(id : number, isEnabled : boolean, adminUserId : st
 DELETE: removeReviewByUserId(userId : string, productId : number)
 */
 
-
-export async function getReviewsByProductId(productId : number) {
+export async function getReviewsByProductId(productId: number) {
   return (await db('reviews')
     .select(
       'product_id as productId',
@@ -26,12 +25,21 @@ export async function getReviewsByProductId(productId : number) {
       'rating',
       'is_enabled as isEnabled',
       'user_id as userId',
-      'created_at as createdAt'
+      'created_at as createdAt',
     )
-    .where('product_id', productId) as Reviews
-  )
+    .where('product_id', productId)) as Reviews
 }
 
+export async function getAmountOfReviewsByDate(
+  date: string,
+  adminUserId: string,
+) {
+  //Check if user is authorised. If they are:
+  //return "User is not authorized"
 
-
-
+  return await db('reviews')
+    .count('* as reviewCount')
+    .whereRaw('DATE(created_at) = ?', date)
+    //DATE will ignore hours/minutes/seconds. It will only look at the year/month/day
+    .first()
+}
