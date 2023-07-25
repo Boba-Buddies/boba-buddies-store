af8d9cded5bcdf9e717ecbff7a04da9b3e339b4a
Sarah Zhong <sarahchungwu@gmail.com>
2023-07-26 11:11:58 +1200

    fixed the shipping option types

--- server/db/shippingOptions.ts
+++ server/db/shippingOptions.ts
@@ -4,7 +4,7 @@ import db from './connection'
 export async function getAllShippingOptions() {
   return (await db('shipping_options').select(
     'id',
-    'shipping_type',
+    'shipping_type as ShippingType',
     'price',
   )) as ShippingOptions[]
 }
