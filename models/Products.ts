062114e0e9b9508503e51aba688aa02cc3722298
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-07-26 10:06:18 +1200

    create OrderSchema

--- models/Products.ts
+++ models/Products.ts
@@ -18,5 +18,28 @@ export const OrdersSchema = z.object({
   purchasedAt: z.string(),
 })
 
+export const OrderSchema = z.object({
+  orderId : z.number(),
+  userFirstName : z.string(),
+  userLastName : z.string(),
+  userAddress : z.string(),
+  userCity : z.string(),
+  userCountry : z.string(),
+  userZipCode : z.string(),
+  userEmail : z.string(),
+  userPhoneNumber : z.string(),
+  totalSale : z.number(),
+  amountOfItems : z.number(),
+  orderDate : z.string(),
+  shippingType : z.string(),
+  shippingPrice : z.number(),
+  orderItems : z.object({
+    productName : z.string(),
+    productSale : z.number(),
+    productImg : z.number(),
+    itemQuantity : z.number(),
+  }).array()
+})
+
 export type UserOrders = z.infer<typeof UserOrdersSchema>
 export type Orders = z.infer<typeof OrdersSchema>
