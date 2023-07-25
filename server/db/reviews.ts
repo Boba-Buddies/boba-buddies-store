685f03c959d307318edf310896ef2c2f3e57c0e7
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-07-26 10:55:22 +1200

    create getReviewsByProductId

--- server/db/reviews.ts
+++ server/db/reviews.ts
@@ -1,10 +1,37 @@
+import { Reviews } from '../../models/Reviews'
+import db from './connection'
+
+/*
+GET: getReviewsByProductId(productId : number)
+
+GET: getAmountOfReviewsByDate(date : format?, adminUserId)
+
+GET: getAllReviews(adminUserId)
+
+GET: getReviewById(id : number, adminUserId : string)
+
+POST: addReviewByUserId(userId : string, review : object)
+
+PATCH: updateReviewStatusById(id : number, isEnabled : boolean, adminUserId : string)
+
+DELETE: removeReviewByUserId(userId : string, productId : number)
+*/
+
+
+export async function getReviewsByProductId(productId : number) {
+  return (await db('reviews')
+    .select(
+      'product_id as productId',
+      'description',
+      'rating',
+      'is_enabled as isEnabled',
+      'user_id as userId',
+      'created_at as createdAt'
+    )
+    .where('product_id', productId) as Reviews
+  )
+}
 
 
 
-//GET: getReviewsByProductId(productId : number)
 
-
-//GET: getAmountOfReviewsByDate(date : format?)
-
-
-//and more please refer back to Figma
