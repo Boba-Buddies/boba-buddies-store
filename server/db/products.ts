2293c249b08cb3c79a26765ab599c51ed9fa014d
Sarah Zhong <sarahchungwu@gmail.com>
2023-07-26 11:37:59 +1200

    getProductById function worked and get product id route worked

--- server/db/products.ts
+++ server/db/products.ts
@@ -14,3 +14,19 @@ export async function getAllProducts() {
     'average_rating as averageRating',
   )) as Products[]
 }
+
+export async function getProductById(id: number) {
+  return (await db('products')
+    .where('id', id)
+    .select(
+      'id',
+      'name',
+      'img',
+      'price',
+      'price',
+      'description',
+      'stock',
+      'is_enabled as isEnabled',
+      'average_rating as averageRating',
+    )) as Products[]
+}
--- server/routes/products.ts
+++ server/routes/products.ts
@@ -14,4 +14,16 @@ router.get('/', async (req, res) => {
   }
 })
 
+// GET /api/v1/products/:id
+router.get('/:id', async (req, res) => {
+  const productId = Number(req.params.id)
+  try {
+    const product = await db.getProductById(productId)
+    res.status(200).json({ product })
+  } catch (error) {
+    logError(error)
+    res.status(500).json({ message: 'Unable to ge the data from database' })
+  }
+})
+
 export default router
