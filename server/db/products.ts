d29141103939d2ccf3055551a227183b226c4201
Sarah Zhong <sarahchungwu@gmail.com>
2023-07-27 11:09:38 +1200

    created the addProduct DB function

--- server/db/products.ts
+++ server/db/products.ts
@@ -1,4 +1,9 @@
-import { LowStockProducts, Product, Products } from '../../models/Products'
+import {
+  LowStockProducts,
+  Product,
+  Products,
+  UpsertProduct,
+} from '../../models/Products'
 import db from './connection'
 
 export async function getAllProducts() {
@@ -35,4 +40,15 @@ export async function getAmountOfProductsBelowStockLevel(maxStock: number) {
   return (await db('products')
     .where('stock', '<', maxStock)
     .select('id', 'name', 'img')) as LowStockProducts[]
+}
+
+export async function addProduct(newProduct: UpsertProduct) {
+  return await db('products').insert({
+    name: newProduct.name,
+    img: newProduct.img,
+    price: newProduct.price,
+    stock: newProduct.stock,
+    description: newProduct.description,
+    is_enabled: newProduct.isEnabled,
+  })
 }
