57c346f136f27fd76c073f20e9cf43cef41c6d79
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-07-27 11:21:10 +1200

    create addCartToPurchasesByUserId

--- server/db/purchases.ts
+++ server/db/purchases.ts
@@ -1,3 +1,4 @@
+import { TransferedCart } from '../../models/Purchases'
 import db from './connection'
 
 export async function getLatestOrderIdByUserId(userId: string) {
@@ -7,4 +8,34 @@ export async function getLatestOrderIdByUserId(userId: string) {
     .first()
 
   return latestPurchase.order_id
+}
+
+export async function getLatestOrderId() {
+  const latestPurchase = await db('purchases')
+    .orderBy('purchased_at', 'desc') // Order the rows by purchased_at in descending order (newest first)
+    .first()
+
+  return latestPurchase.order_id
+}
+
+export async function addCartToPurchasesByUserId(
+  userId: string,
+  transferedCart: TransferedCart,
+) {
+  const latestOrderId: number = await getLatestOrderId()
+  const newOrderId: number = latestOrderId + 1
+  const timestamp = db.fn.now() //get the current time
+
+  //For each object inside transfered cart, insert it to the table, adding the timestamp and newOrderId to each one.
+  const dataArray = transferedCart.map((purchase) => {
+    return {
+      ...purchase,
+      user_id: userId,
+      order_id: newOrderId,
+      purchased_at: timestamp,
+    }
+  })
+
+  await db('purchases').insert(dataArray)
+  //Now clear the user's cart.
 }
