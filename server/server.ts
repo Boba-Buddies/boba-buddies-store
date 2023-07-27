9b38fb52e61962848392f161da2feae706d890bc
Sarah Zhong <sarahchungwu@gmail.com>
2023-07-27 12:13:45 +1200

    udpate the shippingoption route

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import { join } from 'node:path'
-import shippingRoutes from './routes/shippingOptions'
+import shippingOptionsRoutes from './routes/shippingOptions'
 import productsRoutes from './routes/products'
 
 const server = express()
@@ -8,7 +8,7 @@ const server = express()
 server.use(express.json())
 server.use(express.static(join(__dirname, 'public')))
 
-server.use('/api/v1/shippings', shippingRoutes)
+server.use('/api/v1/shipping-options', shippingOptionsRoutes)
 server.use('/api/v1/products', productsRoutes)
 
 export default server
