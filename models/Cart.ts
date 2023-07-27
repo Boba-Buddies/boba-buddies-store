089f32b7637d33f6a754060db7bdcd349234a4c7
Rigelle Cruz <rigellecruz@gmail.com>
2023-07-27 12:07:23 +1200

    update cart & users models schema

--- models/Cart.ts
+++ models/Cart.ts
@@ -1,6 +1,6 @@
 import * as z from 'zod'
 
-export const CartSchema = z
+export const cartSchema = z
   .object({
     image: z.string(),
     name: z.string(),
@@ -10,12 +10,12 @@ export const CartSchema = z
   })
   .array()
 
-export const CartItemSchema = z.object({
+export const cartItemSchema = z.object({
   id: z.number(),
   userId: z.string(),
   productId: z.number(),
   quantity: z.number(),
 })
 
-export type Cart = z.infer<typeof CartSchema>
-export type CartItem = z.infer<typeof CartItemSchema>
+export type Cart = z.infer<typeof cartSchema>
+export type CartItem = z.infer<typeof cartItemSchema>
