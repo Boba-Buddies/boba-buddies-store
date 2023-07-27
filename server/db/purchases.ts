dc9613954826928430c3b953e6311a88d9713f7c
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-07-28 10:13:36 +1200

    fix purchases seed file

--- server/db/purchases.ts
+++ server/db/purchases.ts
@@ -81,3 +81,12 @@ export async function getOrdersByUserId(userId: string) {
 
   return userOrders
 }
+
+export async function getAllOrders() {
+  //returns an array of objects, where each objects represents a summary of every order that looks like this:
+  //{ userName : string, orderId : number, totalSale : number, purchasedAt : timestamp}
+  //we will need use the user_id in the purchases table to link to auth0_id in the users table to get user_name
+  //totalSale will need to be calculated by taking the product_id to get the price from the products table, then multiplying it by the quantity. That needs to be done for every purchase in an order linked by the same order_id, and then you need to add the total from each row in that order to get the totalSale.
+  //since the purchased_at is the same in every row of the same order, we can just get it from the first purchase.
+
+}
