7c8cac2888663bf08be7f0a8e9808b8405efa7f5
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-07-31 11:46:44 +1200

    change order_id to number in migrations

--- server/db/migrations/20230724210411_purchases.js
+++ server/db/migrations/20230724210411_purchases.js
@@ -1,16 +1,23 @@
-exports.up = function(knex) {
-  return knex.schema.createTable('purchases', function(table) {
-    table.increments('id').primary();
-    table.string('user_id').references('users.auth0_id').notNullable();
-    table.integer('product_id').references('products.id').unsigned().notNullable();
-    table.integer('quantity').unsigned().notNullable();
-    table.timestamp('purchased_at').notNullable().defaultTo(knex.fn.now());
-    table.integer('shipping_id').references('shipping_options.id').unsigned().notNullable();
-    table.string('order_id').notNullable();
-  });
-};
+exports.up = function (knex) {
+  return knex.schema.createTable('purchases', function (table) {
+    table.increments('id').primary()
+    table.string('user_id').references('users.auth0_id').notNullable()
+    table
+      .integer('product_id')
+      .references('products.id')
+      .unsigned()
+      .notNullable()
+    table.integer('quantity').unsigned().notNullable()
+    table.timestamp('purchased_at').notNullable().defaultTo(knex.fn.now())
+    table
+      .integer('shipping_id')
+      .references('shipping_options.id')
+      .unsigned()
+      .notNullable()
+    table.number('order_id').notNullable()
+  })
+}
 
-
-exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('purchases');
-};
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('purchases')
+}
