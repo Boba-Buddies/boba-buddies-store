aa614d28b9a8be860ecffaab784e1170719bb119
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-07-31 14:48:39 +1200

    complete setup for reviews routes

--- server/routes/reviews.ts
+++ server/routes/reviews.ts
@@ -3,32 +3,30 @@ import * as db from '../db/purchases'
 import { logError } from '../logger'
 const router = Router()
 
-
-
-//!GET /api/v1/reviews/product/:productId
+//!GET /api/v1/reviews/by-product-id/:productId
 //?Gets all the reviews associated with given productId
 //db.getReviewsByProductId(productId: number)
 
-
-
-//!GET /api/v1/reviews/amount/:date/:adminUserId
+//!GET /api/v1/reviews/amount-by-date/:date/:adminUserId
 //?Gets amount of reviews from given date.
 //db.getAmountOfReviewsByDate(date : string, adminUserId : string)
 
-
-//!GET /api/v1/reviews/:adminUserId
+//!GET /api/v1/reviews/all/:adminUserId
 //?Gets all reviews
 //db.getAllReviews(adminUserId)
-
 
 //!GET /api/v1/reviews/by-review-id/:id/:adminUserId
 //?Get review associated with given id
 //db.getReviewById(id : number, adminUserId : string)
 
-
-//!POST /api/v1/reviews
-//?Adds new review from req.body 
+//!POST /api/v1/reviews/add
+//?Adds new review from req.body
 //db.addReviewByUserId(newReview : { userId : string, productId : number, description : string, rating : number })
 
+//!PATCH api/v1/reviews/status
+//?Modifies review status from given information from req.body
+//db.updateReviewStatusById(updatedReviewStatus : {id : number, isEnabled : boolean, adminUserId : string})
 
-//!PATCH api/v1/reviews/:id/:isEnabled/:adminUserId
+//!DELETE api/v1/reviews/remove/:productId/:userId
+//?Deletes review associated with given productId and userId
+//db.removeReviewByUserId(userId : string, productId : number)
