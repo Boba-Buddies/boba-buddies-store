93ff93c4c4d196e6cf83eb4778586e80036371bb
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-08-09 09:53:29 +1200

    refactor superagent requests for products

--- client/apis/products.ts
+++ client/apis/products.ts
@@ -8,7 +8,8 @@ export async function fetchProductById(id: number) {
     const response = await request.get(`${baseUrl}/${id}`)
     return response.body
   } catch (error) {
-    return { error }
+    console.error('Error fetching product by ID:', (error as Error).message)
+    return { error: (error as Error).message }
   }
 }
 
@@ -17,21 +18,26 @@ export async function fetchAllProducts() {
     const response = await request.get(baseUrl)
     return response.body
   } catch (error) {
-    return { error }
+    console.error('Error fetching all products:', (error as Error).message)
+    return { error: (error as Error).message }
   }
 }
 
-//!ADMIN
+//!ADMIN ONLY FUNCTION
 export async function fetchAmountOfProductsBelowStockLevel(maxStock: number) {
   try {
     const response = await request.get(`${baseUrl}/lowstock/${maxStock}`)
     return response.body
   } catch (error) {
-    return { error }
+    console.error(
+      'Error fetching products below stock level:',
+      (error as Error).message,
+    )
+    return { error: (error as Error).message }
   }
 }
 
-//!ADMIN
+//!ADMIN ONLY FUNCTION
 export async function modifyProductById(
   id: number,
   updatedProduct: UpsertProduct,
@@ -42,16 +48,18 @@ export async function modifyProductById(
       .send(updatedProduct)
     return response.body
   } catch (error) {
-    return { error }
+    console.error('Error modifying product by ID:', (error as Error).message)
+    return { error: (error as Error).message }
   }
 }
 
-//!ADMIN
+//!ADMIN ONLY FUNCTION
 export async function createProduct(newProduct: UpsertProduct) {
   try {
     const response = await request.post(baseUrl).send(newProduct)
     return response.body
   } catch (error) {
-    return { error }
+    console.error('Error creating product:', (error as Error).message)
+    return { error: (error as Error).message }
   }
 }
