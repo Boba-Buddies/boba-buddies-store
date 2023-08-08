43deb539d0eb35a9b8edf19b8bcb192ae9280bf8
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-08-09 09:49:45 +1200

    create superagent requests for products

--- client/apis/products.ts
+++ client/apis/products.ts
@@ -1,9 +1,57 @@
 import request from 'superagent'
+import { UpsertProduct } from '../../models/Products'
 
 const baseUrl = '/api/v1/products'
 
-//fetchProductById(id : number) 
-//fetchAllProducts() 
-//fetchAmountOfProductsBelowStockLevel(maxStock : number, adminUserId) 
-//modifyProductById(id : number, updatedProduct : object, admindUserId : string)
-//createProduct(newProduct : object, adminUserId : string)
+export async function fetchProductById(id: number) {
+  try {
+    const response = await request.get(`${baseUrl}/${id}`)
+    return response.body
+  } catch (error) {
+    return { error }
+  }
+}
+
+export async function fetchAllProducts() {
+  try {
+    const response = await request.get(baseUrl)
+    return response.body
+  } catch (error) {
+    return { error }
+  }
+}
+
+//!ADMIN
+export async function fetchAmountOfProductsBelowStockLevel(maxStock: number) {
+  try {
+    const response = await request.get(`${baseUrl}/lowstock/${maxStock}`)
+    return response.body
+  } catch (error) {
+    return { error }
+  }
+}
+
+//!ADMIN
+export async function modifyProductById(
+  id: number,
+  updatedProduct: UpsertProduct,
+) {
+  try {
+    const response = await request
+      .patch(`${baseUrl}/${id}`)
+      .send(updatedProduct)
+    return response.body
+  } catch (error) {
+    return { error }
+  }
+}
+
+//!ADMIN
+export async function createProduct(newProduct: UpsertProduct) {
+  try {
+    const response = await request.post(baseUrl).send(newProduct)
+    return response.body
+  } catch (error) {
+    return { error }
+  }
+}
