3a375ecbc6b844e39e8c07a8fe1b4cef8b7fc30c
Rigelle Cruz <rigellecruz@gmail.com>
2023-08-09 10:06:21 +1200

    write front-end api for updating item quantity

--- client/apis/cart.ts
+++ client/apis/cart.ts
@@ -9,10 +9,20 @@ export async function fetchCart() {
 }
 
 export async function deleteProductFromCartApi(productId: number) {
-  console.log('Deleting product with ID:', productId)
   const response = await request
     .delete(`${baseUrl}/${productId}`)
     .set('Content-Type', 'application/json')
+
+  return response.body.cart as CartClient[]
+}
+
+export async function modifyCartItemQuantity(
+  productId: number,
+  quantity: number,
+) {
+  const response = await request
+    .patch('/api/update-quantity')
+    .send({ productId, quantity })
 
   return response.body.cart as CartClient[]
 }
--- client/user/pages/Cart/Cart.tsx
+++ client/user/pages/Cart/Cart.tsx
@@ -21,6 +21,11 @@ const Cart = () => {
     }
   }
 
+  const handleUpdateQuantityFromCart = async (
+    productId: number,
+    quantity: number,
+  ) => {}
+
   return (
     <div>
       <h2 className="text-blue-500">Your Cart</h2>
@@ -32,7 +37,31 @@ const Cart = () => {
           <div className="flex-1">
             <h3 className="font-bold">{item.name}</h3>
             <p>Price: ${item.price}</p>
-            <p>Quantity: {item.quantity}</p>
+            <div className="flex items-center mt-2">
+              <button
+                onClick={() =>
+                  handleUpdateQuantityFromCart(
+                    item.productId,
+                    item.quantity - 1,
+                  )
+                }
+                className="px-2 py-1 bg-gray-300 text-gray-600 rounded-full transition-colors hover:bg-gray-400 focus:outline-none focus:ring focus:ring-gray-300"
+              >
+                -
+              </button>
+              <p className="px-3">{item.quantity}</p>
+              <button
+                onClick={() =>
+                  handleUpdateQuantityFromCart(
+                    item.productId,
+                    item.quantity + 1,
+                  )
+                }
+                className="px-2 py-1 bg-gray-300 text-gray-600 rounded-full transition-colors hover:bg-gray-400 focus:outline-none focus:ring focus:ring-gray-300"
+              >
+                +
+              </button>
+            </div>
             <button
               onClick={() => handleDeleteProductFromCart(item.productId)}
               className="mt-3 px-3 py-1 text-sm bg-red-500 text-white rounded-md transition-colors hover:bg-red-600 focus:outline-none focus:ring focus:ring-red-300"
