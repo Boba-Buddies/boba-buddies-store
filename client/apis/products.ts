d80371102a370c0a509eb760b3f29f5a01e86232
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-08-08 12:02:58 +1200

    add imports to products api front end

--- client/apis/products.ts
+++ client/apis/products.ts
@@ -1,5 +1,9 @@
-//fetchProductById(id : number)
-//fetchAllProducts()
-//fetchAmountOfProductsBelowStockLevel(maxStock : number, adminUserId)
+import request from 'superagent'
+
+const baseUrl = '/api/v1/products'
+
+//fetchProductById(id : number) 
+//fetchAllProducts() 
+//fetchAmountOfProductsBelowStockLevel(maxStock : number, adminUserId) 
 //modifyProductById(id : number, updatedProduct : object, admindUserId : string)
 //createProduct(newProduct : object, adminUserId : string)
