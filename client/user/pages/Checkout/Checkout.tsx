10d96aea004d2fb293df770118d3f88a6afccd1f
Sarah Zhong <sarahchungwu@gmail.com>
2023-08-09 09:51:29 +1200

    wrote the input tag for phone and delivery address

--- client/user/pages/Checkout/Checkout.tsx
+++ client/user/pages/Checkout/Checkout.tsx
@@ -1,8 +1,61 @@
-import React from 'react'
-
-const Checkout = () => {
+function Checkout() {
   return (
-    <div>Checkout</div>
+    <>
+      <div>
+        <div>I am the Logo</div>
+        <form action="">
+          <div>
+            <h1>PAYMENT INFORMATION</h1>
+          </div>
+          <div>
+            <input type="text" id="phone" placeholder="PHONE" />
+          </div>
+          <h1>DELIEVERY ADDRESS</h1>
+          <div>
+            <input
+              type="text"
+              name="firtName"
+              id="firstName"
+              placeholder="FIRST NAME"
+            />
+            <input
+              type="text"
+              name="lastName"
+              id="lastName"
+              placeholder="LAST NAME"
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="address"
+              id="address"
+              placeholder="ADDRESS"
+            />
+          </div>
+          <div>
+            <input type="text" name="city" id="city" placeholder="CITY" />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="country"
+              id="country"
+              placeholder="COUNTRY"
+            />
+            <input
+              type="text"
+              name="zipCode"
+              id="zipCode"
+              placeholder="ZIPCODE"
+            />
+          </div>
+          <div>
+            <label htmlFor="">SELECT PAYMENT METHOD</label>
+          </div>
+        </form>
+      </div>
+    </>
   )
 }
 
