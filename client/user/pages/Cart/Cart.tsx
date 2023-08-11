ad3297e1eaae44acbec29636b5c1ef33b7efec96
Rigelle Cruz <rigellecruz@gmail.com>
2023-08-11 14:14:03 +1200

    complete cart styling

--- client/user/pages/Cart/Cart.tsx
+++ client/user/pages/Cart/Cart.tsx
@@ -51,90 +51,97 @@ const Cart = () => {
   return (
     <>
       <LoadError status={status} />
-      <div className="flex justify-center items-center mt-4">
-        <h1 className="text-3xl font-bold tracking-wider">CART</h1>
-      </div>
-
       <div className="flex justify-center items-center min-h-screen mt-4">
-        <div className="flex w-4/5">
-          <div className="w-3/4 pr-6">
-            {data &&
-              data.map((item: CartClient) => (
-                <div
-                  key={item.productId}
-                  className="flex items-center justify-between mb-6 border p-4 rounded-md"
-                >
-                  <div className="flex-shrink-0 w-1/4 pr-4">
-                    <img
-                      src={item.image}
-                      alt={item.name}
-                      className="max-w-full h-auto"
-                    />
-                  </div>
-                  <div className="flex-1">
-                    <h3 className="font-bold">{item.name}</h3>
-                    <p className="text-gray-600">Price: ${item.price}</p>
-                    <div className="flex items-center mt-2">
+        <div className="flex w-4/5 justify-end">
+          <div className="w-3/5 pl-6">
+            <h1 className="text-3xl font-bold tracking-wider">CART</h1>
+            <div className="mt-4">
+              {data &&
+                data.map((item: CartClient) => (
+                  <div
+                    key={item.productId}
+                    className="flex items-center justify-between mb-6 border p-4 rounded-md"
+                  >
+                    <div className="flex-shrink-0 w-1/4 pr-4">
+                      <img
+                        src={item.image}
+                        alt={item.name}
+                        className="max-w-full h-auto"
+                      />
+                    </div>
+                    <div className="flex-1">
+                      <h3 className="font-bold">{item.name}</h3>
+                      <p className="text-gray-600">Price: ${item.price}</p>
+                      <div className="flex items-center mt-2">
+                        <button
+                          onClick={() => {
+                            modifyQuantityMutation.mutate({
+                              productId: item.productId,
+                              quantity: item.quantity - 1,
+                            })
+                          }}
+                          className="px-2 py-1 bg-gray-300 text-gray-600 rounded-full transition-colors hover:bg-gray-400 focus:outline-none focus:ring focus:ring-gray-300"
+                        >
+                          -
+                        </button>
+                        <p className="px-3">{item.quantity}</p>
+                        <button
+                          onClick={() => {
+                            modifyQuantityMutation.mutate({
+                              productId: item.productId,
+                              quantity: item.quantity + 1,
+                            })
+                          }}
+                          className="px-2 py-1 bg-gray-300 text-gray-600 rounded-full transition-colors hover:bg-gray-400 focus:outline-none focus:ring focus:ring-gray-300"
+                        >
+                          +
+                        </button>
+                      </div>
+
                       <button
-                        onClick={() => {
-                          modifyQuantityMutation.mutate({
-                            productId: item.productId,
-                            quantity: item.quantity - 1,
-                          })
-                        }}
-                        className="px-2 py-1 bg-gray-300 text-gray-600 rounded-full transition-colors hover:bg-gray-400 focus:outline-none focus:ring focus:ring-gray-300"
+                        onClick={() =>
+                          deleteProductMutation.mutate(item.productId)
+                        }
+                        className="mt-3 px-3 py-1 text-sm bg-red-500 text-white rounded-md transition-colors hover:bg-red-600 focus:outline-none focus:ring focus:ring-red-300"
                       >
-                        -
-                      </button>
-                      <p className="px-3">{item.quantity}</p>
-                      <button
-                        onClick={() => {
-                          modifyQuantityMutation.mutate({
-                            productId: item.productId,
-                            quantity: item.quantity + 1,
-                          })
-                        }}
-                        className="px-2 py-1 bg-gray-300 text-gray-600 rounded-full transition-colors hover:bg-gray-400 focus:outline-none focus:ring focus:ring-gray-300"
-                      >
-                        +
+                        Remove
                       </button>
                     </div>
-
-                    <button
-                      onClick={() =>
-                        deleteProductMutation.mutate(item.productId)
-                      }
-                      className="mt-3 px-3 py-1 text-sm bg-red-500 text-white rounded-md transition-colors hover:bg-red-600 focus:outline-none focus:ring focus:ring-red-300"
-                    >
-                      Remove
-                    </button>
+                    <p className="font-bold text-right">
+                      $ {(item.price * item.quantity).toFixed(2)}
+                    </p>
                   </div>
-                  <p className="font-bold text-right">
-                    $ {(item.price * item.quantity).toFixed(2)}
-                  </p>
-                </div>
-              ))}
+                ))}
+            </div>
           </div>
 
-          <div className="w-1/5">
-            <div className="border border-gray-300 shadow-md bg-gray-100 p-4 rounded-md">
+          <div className="w-1/3 pl-6 mt-14">
+            <div
+              className="shadow-md bg-black p-8 rounded-md text-white"
+              style={{ width: '340px' }}
+            >
               {data && (
-                <p className="text-gray-600">
-                  Total: $
-                  {data
-                    .reduce(
-                      (total, item) => total + item.price * item.quantity,
-                      0,
-                    )
-                    .toFixed(2)}
+                <p>
+                  <span className="font-bold">Total: </span>
+                  <span className="pl-40">
+                    {' '}
+                    ${' '}
+                    {data
+                      .reduce(
+                        (total, item) => total + item.price * item.quantity,
+                        0,
+                      )
+                      .toFixed(2)}
+                  </span>
                 </p>
               )}
-              <p className="text-gray-600">
-                <span>Shipping: TBC</span>
+              <p>
+                <span className="font-bold">Shipping: </span>
+                <span className="pl-40">TBC</span>
               </p>
               <button
                 onClick={() => goTo('/Checkout')}
-                className="mt-2 px-2 py-1 bg-black text-white rounded-md transition-colors hover:bg-gray-700 focus:outline-none focus:ring focus:ring-black"
+                className="mt-4 w-full py-2 bg-white text-black font-bold rounded-md transition-colors hover:bg-gray-400 hover:text-white focus:outline-none focus:ring focus:ring-black"
               >
                 Checkout
               </button>
