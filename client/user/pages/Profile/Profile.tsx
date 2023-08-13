957dfa81e4805b754eecdb17bab4996a4b946a0a
Rigelle Cruz <rigellecruz@gmail.com>
2023-08-14 10:06:50 +1200

    add auth0Id to user schema model

--- client/user/pages/Profile/Profile.tsx
+++ client/user/pages/Profile/Profile.tsx
@@ -1,7 +1,28 @@
+import { useQuery } from 'react-query'
+
+import { fetchUser } from '../../../apis/users'
+
+import LoadError from '../../components/LoadError/LoadError'
+import { User } from '../../../../models/Users'
+
 const Profile = () => {
+  const { data, status } = useQuery('fetchUser', fetchUser)
+
   return (
     <>
+      <LoadError status={status} />
       <h1 className="text-3xl font-bold tracking-wider">Profile</h1>
+
+      <div>
+        {data &&
+          data.map((userDetails: User) => (
+            <div key={userDetails.id}>
+              {' '}
+              {/* Added 'key' prop for mapping */}
+              {userDetails.firstName}
+            </div>
+          ))}
+      </div>
     </>
   )
 }
