5d8d8fa50c7c47d078766ee43e851cdfbd8b72b8
Rigelle Cruz <rigellecruz@gmail.com>
2023-08-18 09:57:39 +1200

    clean up styling

--- client/user/pages/Profile/Profile.tsx
+++ client/user/pages/Profile/Profile.tsx
@@ -8,38 +8,46 @@ const Profile = () => {
   const { data, status } = useQuery('fetchUser', fetchUser)
 
   return (
-    <>
-      <h1 className="text-3xl font-bold tracking-wider">Profile</h1>
+    <div className="p-8">
+      <h1 className="text-3xl font-bold tracking-wider mb-8">
+        Hello, {data?.firstName} {data?.lastName}!
+      </h1>
+
       <div className="flex">
-        <div className="w-1/2 p-8">
-          <div className="mt-4">
-            <h2 className="text-xl font-semibold">Orders</h2>
-            {/* Render your list of orders here */}
-            <ul className="pl-4">
-              <li className="border p-2 mb-2">Order 1</li>
-              <li className="border p-2 mb-2">Order 2</li>
-              <li className="border p-2 mb-2">Order 3</li>
-              {/* Add more items as needed */}
-            </ul>
-          </div>
+        <div className="w-1/2 pr-8">
+          <h2 className="text-xl font-semibold mb-4">Orders</h2>
+          <ul className="space-y-2">
+            <li className="border p-2">Order 1</li>
+            <li className="border p-2">Order 2</li>
+            <li className="border p-2">Order 3</li>
+            {/* Add more items as needed */}
+          </ul>
         </div>
 
-        <div className="w-1/2 p-8">
+        <div className="w-1/2 pl-8">
+          <h2 className="text-xl font-semibold mb-4">Account Details</h2>
+          <p>
+            {data?.firstName} {data?.lastName}
+          </p>
+          <p>{data?.address}</p>
+          <p>{data?.city}</p>
+          <p>{data?.country}</p>
+          <p>{data?.zipCode}</p>
           <LoadError status={status} />
-
-          <div className="mt-4">
-            <h2 className="text-xl font-semibold">Account Details</h2>
-            <p>
-              {data && data.firstName} {data && data.lastName}
-            </p>
-            <p>{data && data.address}</p>
-            <p>{data && data.city}</p>
-            <p>{data && data.country}</p>
-            <p>{data && data.zipCode}</p>
-          </div>
         </div>
       </div>
-    </>
+
+      <div className="mt-8">
+        <div>
+          <h2 className="text-xl font-semibold mb-4">Reviews</h2>
+          <ul className="space-y-2">
+            <li className="border p-2">Review 1</li>
+            <li className="border p-2">Review 2</li>
+            <li className="border p-2">Review 3</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
 
