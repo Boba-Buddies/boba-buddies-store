28c5038868affe6cbbbd99203587b19b7fd8424d
Rigelle Cruz <rigellecruz@gmail.com>
2023-08-18 09:45:41 +1200

    modify styling

--- client/user/pages/Profile/Profile.tsx
+++ client/user/pages/Profile/Profile.tsx
@@ -9,19 +9,35 @@ const Profile = () => {
 
   return (
     <>
-      <LoadError status={status} />
-
       <h1 className="text-3xl font-bold tracking-wider">Profile</h1>
+      <div className="flex">
+        <div className="w-1/2 p-8">
+          <div className="mt-4">
+            <h2 className="text-xl font-semibold">Orders</h2>
+            {/* Render your list of orders here */}
+            <ul className="pl-4">
+              <li className="border p-2 mb-2">Order 1</li>
+              <li className="border p-2 mb-2">Order 2</li>
+              <li className="border p-2 mb-2">Order 3</li>
+              {/* Add more items as needed */}
+            </ul>
+          </div>
+        </div>
 
-      <div>
-        <h2>Account Details</h2>
-        <p>
-          {data && data.firstName} {data && data.lastName}
-        </p>
-        <p>{data && data.address}</p>
-        <p>{data && data.city}</p>
-        <p>{data && data.country}</p>
-        <p>{data && data.zipCode}</p>
+        <div className="w-1/2 p-8">
+          <LoadError status={status} />
+
+          <div className="mt-4">
+            <h2 className="text-xl font-semibold">Account Details</h2>
+            <p>
+              {data && data.firstName} {data && data.lastName}
+            </p>
+            <p>{data && data.address}</p>
+            <p>{data && data.city}</p>
+            <p>{data && data.country}</p>
+            <p>{data && data.zipCode}</p>
+          </div>
+        </div>
       </div>
     </>
   )
