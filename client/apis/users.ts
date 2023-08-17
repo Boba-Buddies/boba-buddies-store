dcc92d6ae8a4663569965c1135722893dbb583b7
Rigelle Cruz <rigellecruz@gmail.com>
2023-08-18 09:26:52 +1200

    fetching user

--- client/apis/users.ts
+++ client/apis/users.ts
@@ -3,24 +3,13 @@ import { User } from '../../models/Users'
 
 const baseUrl = '/api/v1/users'
 
-// export async function fetchUser() {
-//   try {
-//     const response = await request.get(`${baseUrl}`)
-//     return response.body.users as User[]
-//   } catch (error) {
-//     console.error('An error occurred:', (error as Error).message)
-//     return { error: (error as Error).message }
-//   }
-// }
-
 export async function fetchUser() {
   try {
     const response = await request.get(`${baseUrl}`)
     const userData = response.body as User
-    console.log(userData)
     return userData
   } catch (error) {
     console.error('An error occurred:', (error as Error).message)
-    return { error: (error as Error).message }
+    throw { error: (error as Error).message }
   }
 }
--- client/user/pages/Profile/Profile.tsx
+++ client/user/pages/Profile/Profile.tsx
@@ -3,10 +3,9 @@ import { useQuery } from 'react-query'
 import { fetchUser } from '../../../apis/users'
 
 import LoadError from '../../components/LoadError/LoadError'
-import { User } from '../../../../models/Users'
 
 const Profile = () => {
-  const { data: User, status } = useQuery('fetchUser', fetchUser)
+  const { data, status } = useQuery('fetchUser', fetchUser)
 
   return (
     <>
@@ -15,10 +14,7 @@ const Profile = () => {
       <h1 className="text-3xl font-bold tracking-wider">Profile</h1>
 
       <div>
-        {/* {data &&
-          data.map((userDetails: User) => (
-            <p key={userDetails.userId}> {userDetails.firstName}</p>
-          ))} */}
+        <p>{data && data.firstName}</p>
       </div>
     </>
   )
