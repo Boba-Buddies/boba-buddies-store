9b188279a07c09f4a1c5cadab21d4296b58fce62
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-08-18 11:28:12 +1200

    create add to cart functionality for ViewProduct

--- client/user/components/ViewProduct/ViewProduct.tsx
+++ client/user/components/ViewProduct/ViewProduct.tsx
@@ -1,11 +1,34 @@
+import { useState } from 'react'
 import { Product } from '../../../../models/Products'
+import { addProductToCart } from '../../../apis/cart'
 import StarRating from '../StarRating/StarRating'
+import { useMutation } from 'react-query'
 
 interface ViewProductProps {
   product: Product
 }
 
 function ViewProduct({ product }: ViewProductProps) {
+  const mutation = useMutation((productId: number) => addProductToCart(productId))
+  const [buttonText, setButtonText] = useState('Add to cart')
+  const [buttonColor, setButtonColor] = useState('bg-blue-500 hover:bg-blue-700')
+
+
+  const handleAddToCart = () => {
+    mutation.mutate(product.id, {
+      onSuccess: () => {
+        setButtonText('Item added')
+        setButtonColor('bg-gray-500')
+        
+        setTimeout(() => {
+          setButtonText('Add to cart')
+          setButtonColor('bg-blue-500 hover:bg-blue-700')
+        }, 1000)
+      },
+    })
+  }
+  
+
   return (
     <div
       className="flex items-center max-w-5xl"
@@ -23,9 +46,14 @@ function ViewProduct({ product }: ViewProductProps) {
         </div>
 
         <p className="mt-2">{product.description}</p>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-2 rounded">
-          Add to cart
-        </button>
+        <button
+        className={`${buttonColor} text-white font-bold py-2 px-4 mt-2 rounded`}
+        onClick={handleAddToCart}
+        disabled={mutation.isLoading}
+      >
+        {buttonText}
+      </button>
+      {mutation.isError ? <p>Error adding product to cart</p> : null}
       </div>
     </div>
   )
