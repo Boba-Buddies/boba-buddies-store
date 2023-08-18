9cfeeaeff1639a0d76c873e6265d6f9d3d6c1518
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-08-18 12:07:46 +1200

    reformat ViewProduct component

--- client/user/components/ViewProduct/ViewProduct.tsx
+++ client/user/components/ViewProduct/ViewProduct.tsx
@@ -9,17 +9,20 @@ interface ViewProductProps {
 }
 
 function ViewProduct({ product }: ViewProductProps) {
-  const mutation = useMutation((productId: number) => addProductToCart(productId))
+  const mutation = useMutation((productId: number) =>
+    addProductToCart(productId),
+  )
   const [buttonText, setButtonText] = useState('Add to cart')
-  const [buttonColor, setButtonColor] = useState('bg-blue-500 hover:bg-blue-700')
-
+  const [buttonColor, setButtonColor] = useState(
+    'bg-blue-500 hover:bg-blue-700',
+  )
 
   const handleAddToCart = () => {
     mutation.mutate(product.id, {
       onSuccess: () => {
         setButtonText('Item added')
         setButtonColor('bg-gray-500')
-        
+
         setTimeout(() => {
           setButtonText('Add to cart')
           setButtonColor('bg-blue-500 hover:bg-blue-700')
@@ -27,13 +30,9 @@ function ViewProduct({ product }: ViewProductProps) {
       },
     })
   }
-  
 
   return (
-    <div
-      className="flex items-center max-w-5xl"
-      style={{ padding: '10px' }}
-    >
+    <div className="flex items-center max-w-5xl" style={{ padding: '10px' }}>
       <div className="w-1/2">
         <img src={product.image} alt={product.name} className="w-full" />
       </div>
@@ -47,13 +46,13 @@ function ViewProduct({ product }: ViewProductProps) {
 
         <p className="mt-2">{product.description}</p>
         <button
-        className={`${buttonColor} text-white font-bold py-2 px-4 mt-2 rounded`}
-        onClick={handleAddToCart}
-        disabled={mutation.isLoading}
-      >
-        {buttonText}
-      </button>
-      {mutation.isError ? <p>Error adding product to cart</p> : null}
+          className={`${buttonColor} text-white font-bold py-2 px-4 mt-2 rounded`}
+          onClick={handleAddToCart}
+          disabled={mutation.isLoading}
+        >
+          {buttonText}
+        </button>
+        {mutation.isError ? <p>Error adding product to cart</p> : null}
       </div>
     </div>
   )
