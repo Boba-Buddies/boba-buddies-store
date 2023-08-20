5b04e1c91357e0f04b924644f0b55b6b999fdecc
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-08-21 11:42:43 +1200

    fix error of fetching wishlist status in ProductPage

--- client/user/pages/ProductPage/ProductPage.tsx
+++ client/user/pages/ProductPage/ProductPage.tsx
@@ -1,8 +1,7 @@
 import { useQuery } from 'react-query'
 import { useParams } from 'react-router-dom'
 import { fetchProductById } from '../../../apis/products'
-import {
-  fetchReviewsByProductId} from '../../../apis/reviews'
+import { fetchReviewsByProductId } from '../../../apis/reviews'
 import ViewProduct from '../../components/ViewProduct/ViewProduct'
 import LoadError from '../../components/LoadError/LoadError'
 import ViewProductReviews from '../../components/ViewProductReviews/ViewProductReviews'
@@ -12,31 +11,48 @@ import { fetchWishlistStatusByProductId } from '../../../apis/wishlist'
 const ProductPage = () => {
   const params = useParams()
   const id = Number(params.id)
-  const { data: product, status : productStatus } = useQuery(['getProduct', id], async () => {
-    return await fetchProductById(id)
-  })
+  const { data: product, status: productStatus } = useQuery(
+    ['getProduct', id],
+    async () => {
+      return await fetchProductById(id)
+    },
+  )
 
-  const { data: reviews, refetch : refetchReviews, status : reviewStatus } = useQuery(['getReviews', id], async () => {
+  const {
+    data: reviews,
+    refetch: refetchReviews,
+    status: reviewStatus,
+  } = useQuery(['getReviews', id], async () => {
     const fetchedReviews: ProductReviews = await fetchReviewsByProductId(id)
     return fetchedReviews
   })
-  
-  const { data: wishlistProductStatus, refetch : refetchWishlistProductStatus, status : wishlistStatus } = useQuery(['getWishlistStatus', id], async () => {
-    const fetchedReviews: ProductReviews = await fetchReviewsByProductId(id)
-    return fetchedReviews
-  })
 
+  const {
+    data: wishlistStatus = false,
+    refetch: refetchWishlistProductStatus
+  } = useQuery(['getWishlistStatus', id], async () => {
+    const wishlistStatus: boolean = await fetchWishlistStatusByProductId(id)
+      return wishlistStatus
+  })
 
   return (
     <>
-      <LoadError status={[productStatus, reviewStatus, wishlistStatus]} />
+      <LoadError status={[productStatus, reviewStatus]} />
       {product && reviews && (
         <div
           className="flex flex-col items-center w-full"
           style={{ marginTop: '100px', marginBottom: '150px' }}
         >
-          <ViewProduct product={product} wishlistProductStatus = {wishlistProductStatus} />
-          <ViewProductReviews product={product} reviews={reviews} refetch={refetchReviews}/>
+          <ViewProduct
+            product={product}
+            wishlistStatus={wishlistStatus}
+            refetchWishlistProductStatus={refetchWishlistProductStatus}
+          />
+          <ViewProductReviews
+            product={product}
+            reviews={reviews}
+            refetchReviews={refetchReviews}
+          />
         </div>
       )}
     </>
