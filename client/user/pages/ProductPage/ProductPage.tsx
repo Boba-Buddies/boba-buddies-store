f1bd6b74a80c3627944a41c5f8b22d3d6b97bc5e
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-08-21 11:20:07 +1200

    call wishlist product status api in ProductPage component

--- client/user/pages/ProductPage/ProductPage.tsx
+++ client/user/pages/ProductPage/ProductPage.tsx
@@ -7,30 +7,36 @@ import ViewProduct from '../../components/ViewProduct/ViewProduct'
 import LoadError from '../../components/LoadError/LoadError'
 import ViewProductReviews from '../../components/ViewProductReviews/ViewProductReviews'
 import { ProductReviews } from '../../../../models/Reviews'
+import { fetchWishlistStatusByProductId } from '../../../apis/wishlist'
 
 const ProductPage = () => {
   const params = useParams()
   const id = Number(params.id)
-
-  const { data: product, status } = useQuery(['getProduct', id], async () => {
+  const { data: product, status : productStatus } = useQuery(['getProduct', id], async () => {
     return await fetchProductById(id)
   })
 
-  const { data: reviews, refetch } = useQuery(['getReviews', id], async () => {
+  const { data: reviews, refetch : refetchReviews, status : reviewStatus } = useQuery(['getReviews', id], async () => {
+    const fetchedReviews: ProductReviews = await fetchReviewsByProductId(id)
+    return fetchedReviews
+  })
+  
+  const { data: wishlistProductStatus, refetch : refetchWishlistProductStatus, status : wishlistStatus } = useQuery(['getWishlistStatus', id], async () => {
     const fetchedReviews: ProductReviews = await fetchReviewsByProductId(id)
     return fetchedReviews
   })
 
+
   return (
     <>
-      <LoadError status={status} />
+      <LoadError status={[productStatus, reviewStatus, wishlistStatus]} />
       {product && reviews && (
         <div
           className="flex flex-col items-center w-full"
           style={{ marginTop: '100px', marginBottom: '150px' }}
         >
-          <ViewProduct product={product} />
-          <ViewProductReviews product={product} reviews={reviews} refetch={refetch}/>
+          <ViewProduct product={product} wishlistProductStatus = {wishlistProductStatus} />
+          <ViewProductReviews product={product} reviews={reviews} refetch={refetchReviews}/>
         </div>
       )}
     </>
