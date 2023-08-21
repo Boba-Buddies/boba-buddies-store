ed14ab505ae1c7bc0d10aa79acfe751ff73f6e07
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-08-22 10:56:05 +1200

    display all products/product details on the Shop component

--- client/user/pages/Shop/Shop.tsx
+++ client/user/pages/Shop/Shop.tsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import { useQuery } from 'react-query'
+import { fetchAllProductsUser } from '../../../apis/products'
+import StarRating from '../../components/StarRating/StarRating'
+import LoadError from '../../components/LoadError/LoadError'
 
 const Shop = () => {
+  const { data: products, status: statusProducts } = useQuery(
+    ['getAllProducts'],
+    async () => {
+      return await fetchAllProductsUser()
+    },
+  )
+
   return (
-    <div>Shop</div>
+    <>
+      <LoadError status={statusProducts} />
+      {products && (
+        <div className="grid grid-cols-3 gap-4">
+          {products.map((product) => (
+            <div
+              key={product.id}
+              className="border p-4 rounded-md"
+              style={{ maxWidth: '300px' }}
+            >
+              <img
+                src={product.image}
+                alt={product.name}
+                className="w-full h-48 object-contain"
+              />
+              <h3 className="text-xl font-bold mt-2">{product.name}</h3>
+              <p className="text-lg text-gray-600">
+                ${product.price.toFixed(2)}
+              </p>
+              <div className="flex items-center mt-2">
+                <span className="text-yellow-400">
+                  <StarRating rating={product.averageRating} size={1.5} />
+                </span>
+                <span className="ml-2 text-sm text-gray-500">
+                  ({product.averageRating})
+                </span>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
   )
 }
 
