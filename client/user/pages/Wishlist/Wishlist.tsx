bd16659e2dd3c87e534ff928d0a8210ae817105d
Sarah Zhong <sarahchungwu@gmail.com>
2023-08-22 09:56:40 +1200

    created the layout for the tag

--- client/user/pages/Wishlist/Wishlist.tsx
+++ client/user/pages/Wishlist/Wishlist.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-regular-svg-icons'
 
 const Wishlist = () => {
   return (
-    <div>Wishlist</div>
+    <div className="bg-zinc-700 w-full flex flex-col items-center">
+      <div className="w-10/12">
+        <h1 className="">WISHLIST</h1>
+      </div>
+      <div className="bg-lime-300 w-10/12">
+        <div className="bg-sky-400 w-10/12">
+          <img src="" alt="" />
+          <div>
+            <h1>Name</h1>
+            <p>Subtile</p>
+          </div>
+          <h1>Price</h1>
+          <button>Add to Busket</button>
+          <div>
+            <FontAwesomeIcon icon={faHeart} />
+            <button>Remove</button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
