96f029878394c13186482d29873297fd78e4ba7e
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-08-21 12:20:44 +1200

    resize wishlist elements

--- client/user/components/ViewProduct/ViewProduct.tsx
+++ client/user/components/ViewProduct/ViewProduct.tsx
@@ -47,20 +47,20 @@ function ViewProduct({
   const wishlistMutation = useMutation(
     () => {
       if (wishlistStatus) {
-        return deleteFromWishlistByProductId(product.id);
+        return deleteFromWishlistByProductId(product.id)
       } else {
-        return addToWishlistByProductId(product.id);
+        return addToWishlistByProductId(product.id)
       }
     },
     {
       onSuccess: () => refetchWishlistProductStatus(),
-    }
-  );
+    },
+  )
 
   const handleWishlistClick = () => {
-    wishlistMutation.mutate();
+    wishlistMutation.mutate()
   }
-  
+
   return (
     <div className="flex items-center max-w-5xl" style={{ padding: '10px' }}>
       <div className="w-1/2">
@@ -69,16 +69,21 @@ function ViewProduct({
       <div className="w-1/2 ml-4">
         <div className="flex items-center">
           <h1 className="text-3xl font-bold">{product.name}</h1>
-          <button onClick={handleWishlistClick}>
+          <button
+            className="flex items-center" // Added flex and items-center here
+            onClick={handleWishlistClick}
+          >
             <FontAwesomeIcon
               icon={wishlistStatus ? solidHeart : regularHeart}
               className={wishlistStatus ? 'text-red-500' : 'text-black'}
+              style={{ fontSize: '1.875rem', marginLeft: '10px' }}
             />
-            <span className="ml-2">
+            <span className="ml-2 self-center">
               {wishlistStatus ? 'Remove from wishlist' : 'Add to wishlist'}
             </span>
           </button>
         </div>
+
         <h2 className="text-xl font-bold">${product.price}</h2>
         <div className="flex">
           <StarRating rating={product.averageRating} size={1} />
