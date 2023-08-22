bfb880f41d3dda1e75e623234f822b129770db75
Sarah Zhong <sarahchungwu@gmail.com>
2023-08-23 10:25:58 +1200

    fixed the styling for the wishlist

--- client/user/pages/Wishlist/Wishlist.tsx
+++ client/user/pages/Wishlist/Wishlist.tsx
@@ -10,9 +10,12 @@ import { WishlistProduct } from '../../../../models/Wishlist'
 import { fetchCart, modifyCartProductQuantity } from '../../../apis/cart'
 
 const Wishlist = () => {
+  const queryClient = useQueryClient()
+
+  //fetch query
   const wishListQuery = useQuery('fetchWishlist', fetchWishlist)
   const cartQuery = useQuery('fetchCart', fetchCart)
-  const queryClient = useQueryClient()
+
   const statuses = [wishListQuery.status, cartQuery.status]
 
   //add to the cart mutation
@@ -50,37 +53,41 @@ const Wishlist = () => {
   return (
     <>
       <LoadError status={statuses} />
-      <div className="bg-zinc-700 w-full flex flex-col items-center">
-        <div className="w-10/12">
-          <h1 className="">WISHLIST</h1>
+      <div className="bg-white w-full flex flex-col items-center py-8">
+        <div className="w-10/12 text-center mb-4">
+          <h1 className="text-4xl font-semibold text-black">WISHLIST</h1>
         </div>
-        <div className="bg-lime-300 w-10/12">
+        <div className="bg-white w-10/12">
           {!wishListQuery.isLoading &&
             wishListQuery.data &&
             wishListQuery.data.map((item: WishlistProduct) => (
               <div
                 key={item.id}
-                className="bg-sky-400 w-10/12 flex flex-row gap-10"
+                className="bg-white w-10/12 flex flex-row gap-10 items-center border-b border-gray-300 py-4"
               >
                 <img
                   src={item.productImage}
                   alt={item.productName}
-                  className="w-3/12"
+                  className="w-3/12 object-cover"
                 />
-                <h1>{item.productName}</h1>
-                <h1>${item.productPrice.toFixed(2)}</h1>
+                <h1 className="text-xl font-medium text-black w-3/12">
+                  {item.productName}
+                </h1>
+                <h1 className="text-xl font-semibold text-black w-1/12">
+                  ${item.productPrice.toFixed(2)}
+                </h1>
                 <button
-                  className="w-1/6"
+                  className="w-1/6 text-sm bg-black text-white p-2 rounded-md hover:bg-gray-700 transition"
                   onClick={() => handleCartDetails(item.productId)}
                 >
                   Add to Cart
                 </button>
                 <button
-                  className="flex flex-col items-center"
+                  className="flex flex-col items-center text-black hover:text-red-500 transition"
                   onClick={() => removeFromWishList(item.productId)}
                 >
-                  <FontAwesomeIcon icon={faHeart} />
-                  <p>Remove</p>
+                  <FontAwesomeIcon icon={faHeart} className="text-2xl" />
+                  <p className="text-sm mt-1">Remove</p>
                 </button>
               </div>
             ))}
