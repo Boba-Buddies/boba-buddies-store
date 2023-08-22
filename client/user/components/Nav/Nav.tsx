34c1fc2ba30ee40786b555ff7476b907ac2ee3d2
Sarah Zhong <sarahchungwu@gmail.com>
2023-08-23 11:38:39 +1200

    add the link to the button and fixed the tailwind setup in index

--- client/user/components/Nav/Nav.tsx
+++ client/user/components/Nav/Nav.tsx
@@ -1,15 +1,21 @@
+import { useNavigate } from 'react-router-dom'
+
 const Nav = () => {
+  const navigate = useNavigate()
+  function goTo(link: string) {
+    navigate(link)
+  }
   return (
     <div className="bg-red-300 flex flex-row justify-between">
       <div className="w-1/5">
         <img src="/images/bobaLogo.png" alt="logo" />
       </div>
       <div className="flex flex-row gap-7 mr-8">
-        <button>Home</button>
-        <button>Shop</button>
-        <button>Cart</button>
-        <button>Wishlist</button>
-        <button>Account</button>
+        <button onClick={() => goTo('/')}>Home</button>
+        <button onClick={() => goTo('shop')}>Shop</button>
+        <button onClick={() => goTo('cart')}>Cart</button>
+        <button onClick={() => goTo('wishlist')}>WishList</button>
+        <button onClick={() => goTo('profile')}>Account</button>
         <button>Login</button>
       </div>
     </div>
