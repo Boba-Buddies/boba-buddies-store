21b97e9fb25a6da44471c163f18f03c0ebb06fef
Sarah Zhong <sarahchungwu@gmail.com>
2023-08-23 11:18:47 +1200

    added the layout for the Nav

--- client/user/components/Nav/Nav.tsx
+++ client/user/components/Nav/Nav.tsx
@@ -1,5 +1,19 @@
 const Nav = () => {
-  return <div className="bg-red-300">This is the Nav</div>
+  return (
+    <div className="bg-red-300 flex flex-row justify-between">
+      <div className="w-1/5">
+        <img src="/images/bobaLogo.png" alt="logo" />
+      </div>
+      <div className="flex flex-row gap-7 mr-8">
+        <button>Home</button>
+        <button>Shop</button>
+        <button>Cart</button>
+        <button>Wishlist</button>
+        <button>Account</button>
+        <button>Login</button>
+      </div>
+    </div>
+  )
 }
 
 export default Nav
