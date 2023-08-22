a02101240ec0baac076e126e27487e783e534704
Sarah Zhong <sarahchungwu@gmail.com>
2023-08-22 12:45:43 +1200

    fixed the quantity issue in the mutation

--- client/user/pages/Wishlist/Wishlist.tsx
+++ client/user/pages/Wishlist/Wishlist.tsx
@@ -4,40 +4,40 @@ import { useMutation, useQuery, useQueryClient } from 'react-query'
 import { fetchWishlist } from '../../../apis/wishlist'
 import LoadError from '../../components/LoadError/LoadError'
 import { WishlistProduct } from '../../../../models/Wishlist'
-import { modifyCartProductQuantity } from '../../../apis/cart'
+import { fetchCart, modifyCartProductQuantity } from '../../../apis/cart'
 
 const Wishlist = () => {
-  const { isLoading, data, status } = useQuery('fetchWishlist', fetchWishlist)
+  const wishListQuery = useQuery('fetchWishlist', fetchWishlist)
+  const cartQuery = useQuery('fetchCart', fetchCart)
   const queryClient = useQueryClient()
+  const statuses = [wishListQuery.status, cartQuery.status]
   const cartMutation = useMutation(
     ({ productId, quantity }: { productId: number; quantity: number }) =>
       modifyCartProductQuantity(productId, quantity),
     {
       onSuccess: async () => {
-        console.log('success!!! in the mutation')
-
         queryClient.invalidateQueries('fetchCart')
       },
     },
   )
 
   function handleCartDetails(productId: number) {
-    const quantity = 1
-    console.log(productId, 'I am the productId')
-
+    const product = cartQuery.data?.find((item) => item.productId === productId)
+    const quantity = product ? product.quantity + 1 : 1
     cartMutation.mutate({ productId, quantity })
   }
+
   return (
     <>
-      <LoadError status={status} />
+      <LoadError status={statuses} />
       <div className="bg-zinc-700 w-full flex flex-col items-center">
         <div className="w-10/12">
           <h1 className="">WISHLIST</h1>
         </div>
         <div className="bg-lime-300 w-10/12">
-          {!isLoading &&
-            data &&
-            data.map((item: WishlistProduct) => (
+          {!wishListQuery.isLoading &&
+            wishListQuery.data &&
+            wishListQuery.data.map((item: WishlistProduct) => (
               <div
                 key={item.id}
                 className="bg-sky-400 w-10/12 flex flex-row gap-10"
