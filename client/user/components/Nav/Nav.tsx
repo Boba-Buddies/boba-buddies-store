d0f7c8a4dd62cc2c7b5d732510a4fd6aba2a5708
Sarah Zhong <sarahchungwu@gmail.com>
2023-08-24 09:32:43 +1200

    fixed the style of the nav bar

--- client/user/components/Nav/Nav.tsx
+++ client/user/components/Nav/Nav.tsx
@@ -14,39 +14,39 @@ const Nav = () => {
       </div>
       <div className="flex flex-row justify-end gap-6 text-sm mr-7">
         <button
-          className="text-black border-2 border-black px-4 py-1 hover:shadow-lg transition-all ease-in-out duration-300"
+          className="text-white bg-black border rounded border-slate-900 px-4 py-1 hover:shadow-lg transition-all ease-in-out duration-300"
           onClick={() => goTo('/')}
         >
           Home
         </button>
 
         <button
-          className="text-black border-2 border-black px-4 py-1 hover:shadow-lg transition-all ease-in-out duration-300"
+          className="text-white bg-black border rounded border-slate-900 px-4 py-1 hover:shadow-lg transition-all ease-in-out duration-300"
           onClick={() => goTo('shop')}
         >
           Shop
         </button>
         <button
-          className="text-black border-2 border-black px-4 py-1 hover:shadow-lg transition-all ease-in-out duration-300"
+          className="text-white bg-black border rounded border-slate-900 px-4 py-1 hover:shadow-lg transition-all ease-in-out duration-300"
           onClick={() => goTo('cart')}
         >
           Cart
         </button>
+        <button
+          className="text-black px-4 py-1 hover:text-rose-600 transition-all ease-in-out duration-300"
+          onClick={() => goTo('wishlist')}
+        >
+          <FontAwesomeIcon icon={faHeart} className="text-2xl" />
+        </button>
 
         <button
-          className="text-black border-2 border-black px-4 py-1 hover:shadow-lg transition-all ease-in-out duration-300"
+          className="text-white bg-black border rounded border-slate-900 px-4 py-1 hover:shadow-lg transition-all ease-in-out duration-300"
           onClick={() => goTo('profile')}
         >
           Account
         </button>
-        <button className="text-black border-2 border-black px-4 py-1 hover:shadow-lg transition-all ease-in-out duration-300">
+        <button className="text-white bg-black border rounded border-slate-900 px-4 py-1 hover:shadow-lg transition-all ease-in-out duration-300">
           Login
-        </button>
-        <button
-          className="text-black  px-4 py-1 hover:text-rose-600 transition-all ease-in-out duration-300"
-          onClick={() => goTo('wishlist')}
-        >
-          <FontAwesomeIcon icon={faHeart} className="text-2xl" />
         </button>
       </div>
     </div>
