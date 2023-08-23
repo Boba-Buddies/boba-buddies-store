8caa0eed0a71106d24309263912ecefbb4f6896c
Jane Zhang <jiejanezhang2012@gmail.com>
2023-08-23 12:30:00 +1200

    fixed some issues

--- client/user/pages/Contact/Contact.tsx
+++ client/user/pages/Contact/Contact.tsx
@@ -7,18 +7,27 @@ function Contact() {
 
   const mutations = useMutation(createNewEmail)
 
-  const [newEmail, setNewEmail] = useState({} as NewEmail)
+  const [newEmail, setNewEmail] = useState({ title: '', description: '' } as NewEmail)
 
   function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-    const name = event.target.name
-    const value = event.target.value
-    setNewEmail({ ...newEmail, [name]: value })
+    const { name, value } = event.target
+
+    setNewEmail((prevEmail) => ({ ...newEmail, [name]: value }))
   }
 
   function handleSubmit(event: FormEvent) {
     event.preventDefault()
-    mutations.mutate(newEmail)
-    setNewEmail({} as NewEmail)
+
+    if (!newEmail.title || !newEmail.description) {
+      alert('Title and description both are required')
+      return
+    }
+
+    mutations.mutate(newEmail, {
+      onSuccess: () => {
+        setNewEmail({ title: '', description: '' })
+      }
+    })
   }
 
 
@@ -27,9 +36,9 @@ function Contact() {
     <>
       <form className='flex flex-col'>
         <label htmlFor="title" className='mt-8 ml-2'>Title</label>
-        <input className='mt-2 ml-2 border rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500' type="text" name='title' onChange={handleChange} required />
+        <input className='mt-2 ml-2 border rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500' type="text" name='title' onChange={handleChange} value={newEmail.title} required />
         <label htmlFor="description" className='mt-8 ml-2'>Message(description)</label>
-        <input className='mt-6 ml-2 h-28 border rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500' name="description" onChange={handleChange} required />
+        <input className='mt-6 ml-2 h-28 border rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500' name="description" onChange={handleChange} value={newEmail.description} required />
         <button className='bg-pink-400 hover:bg-yellow-300 text-light-blue-300 font-semibold py-2 px-4 rounded-md transition duration-300 ease-in-out mt-6' type='button' onClick={handleSubmit}>Submit</button>
       </form>
     </>
