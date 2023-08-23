805e298ff837bb4cfc21e3a113c2fda34771afcb
Sarah Zhong <sarahchungwu@gmail.com>
2023-08-24 11:06:54 +1200

    finished the initial layout for the home page

--- client/user/pages/Home/Home.tsx
+++ client/user/pages/Home/Home.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+  const navigate = useNavigate()
+  function goTo(link: string) {
+    navigate(link)
+  }
   return (
-    <div>Home</div>
+    <div>
+      <div>
+        <div>
+          <h1>Welcome to the Boba Buddies Store</h1>
+          <button onClick={() => goTo('shop')}>Buy some tea</button>
+        </div>
+      </div>
+      <div>
+        <div>
+          <img src="/images/bobaLogo.png" alt="" />
+        </div>
+        <div>
+          <button>Contact</button>
+          <button>Admin login</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
