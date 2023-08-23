5a1b52393ee2fefb4db8fab3728d3f21eab2978e
Jane Zhang <jiejanezhang2012@gmail.com>
2023-08-23 12:02:52 +1200

    commit before pull

--- client/user/pages/Contact/Contact.tsx
+++ client/user/pages/Contact/Contact.tsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import { FormEvent, useState } from 'react'
+import { QueryClient, useMutation, useQueryClient } from 'react-query'
+import { NewEmail } from '../../../../models/Emails'
+import { createNewEmail } from '../../../apis/emails'
 
-const Contact = () => {
-  return <div>On the contact page</div>
+function Contact() {
+
+  const mutations = useMutation(createNewEmail)
+
+  const [newEmail, setNewEmail] = useState({} as NewEmail)
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const name = event.target.name
+    const value = event.target.value
+    setNewEmail({ ...newEmail, [name]: value })
+  }
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+    mutations.mutate(newEmail)
+    setNewEmail({} as NewEmail)
+  }
+
+
+
+  return (
+    <>
+      <form className='flex flex-col'>
+        <label htmlFor="title" className='mt-8 ml-2'>Title</label>
+        <input className='mt-2 ml-2 border rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500' type="text" name='title' onChange={handleChange} required />
+        <label htmlFor="description" className='mt-8 ml-2'>Message(description)</label>
+        <input className='mt-6 ml-2 h-28 border rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500' name="description" onChange={handleChange} required />
+        <button className='bg-pink-400 hover:bg-yellow-300 text-light-blue-300 font-semibold py-2 px-4 rounded-md transition duration-300 ease-in-out mt-6' type='button' onClick={handleSubmit}>Submit</button>
+      </form>
+    </>
+  )
 }
 
 export default Contact
