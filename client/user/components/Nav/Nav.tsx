e96122b086d2081cce8e9326d15518516c6b1126
Sarah Zhong <sarahchungwu@gmail.com>
2023-08-23 12:36:05 +1200

    the styling for the nav done

--- client/user/components/Nav/Nav.tsx
+++ client/user/components/Nav/Nav.tsx
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-regular-svg-icons'
 import { useNavigate } from 'react-router-dom'
 
 const Nav = () => {
@@ -6,17 +8,46 @@ const Nav = () => {
     navigate(link)
   }
   return (
-    <div className="bg-red-300 flex flex-row justify-between">
-      <div className="w-1/5">
-        <img src="/images/bobaLogo.png" alt="logo" />
+    <div className="bg-white flex flex-row justify-end items-center py-4 px-8">
+      <div className="w-1/4 my-2">
+        <img src="/images/bobaLogo.png" alt="logo" className="w-11/12 px-8" />
       </div>
-      <div className="flex flex-row gap-7 mr-8">
-        <button onClick={() => goTo('/')}>Home</button>
-        <button onClick={() => goTo('shop')}>Shop</button>
-        <button onClick={() => goTo('cart')}>Cart</button>
-        <button onClick={() => goTo('wishlist')}>WishList</button>
-        <button onClick={() => goTo('profile')}>Account</button>
-        <button>Login</button>
+      <div className="flex flex-row justify-end gap-6 text-sm mr-7">
+        <button
+          className="text-black border-2 border-black px-4 py-1 hover:shadow-lg transition-all ease-in-out duration-300"
+          onClick={() => goTo('/')}
+        >
+          Home
+        </button>
+
+        <button
+          className="text-black border-2 border-black px-4 py-1 hover:shadow-lg transition-all ease-in-out duration-300"
+          onClick={() => goTo('shop')}
+        >
+          Shop
+        </button>
+        <button
+          className="text-black border-2 border-black px-4 py-1 hover:shadow-lg transition-all ease-in-out duration-300"
+          onClick={() => goTo('cart')}
+        >
+          Cart
+        </button>
+
+        <button
+          className="text-black border-2 border-black px-4 py-1 hover:shadow-lg transition-all ease-in-out duration-300"
+          onClick={() => goTo('profile')}
+        >
+          Account
+        </button>
+        <button className="text-black border-2 border-black px-4 py-1 hover:shadow-lg transition-all ease-in-out duration-300">
+          Login
+        </button>
+        <button
+          className="text-black  px-4 py-1 hover:text-rose-600 transition-all ease-in-out duration-300"
+          onClick={() => goTo('wishlist')}
+        >
+          <FontAwesomeIcon icon={faHeart} className="text-2xl" />
+        </button>
       </div>
     </div>
   )
