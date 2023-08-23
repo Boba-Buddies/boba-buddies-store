30b4bc64fd69ee865d82a33d9dce233dace26a3e
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-08-24 10:47:53 +1200

    add styling for pagination interface on Shop

--- client/user/pages/Shop/Shop.tsx
+++ client/user/pages/Shop/Shop.tsx
@@ -1,90 +1,98 @@
-import { useQuery } from 'react-query';
-import { fetchAllProductsUser } from '../../../apis/products';
-import StarRating from '../../components/StarRating/StarRating';
-import LoadError from '../../components/LoadError/LoadError';
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import SortFilterControls from '../../components/SortFilterControls/SortFilterControls';
+import { useQuery } from 'react-query'
+import { fetchAllProductsUser } from '../../../apis/products'
+import StarRating from '../../components/StarRating/StarRating'
+import LoadError from '../../components/LoadError/LoadError'
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import SortFilterControls from '../../components/SortFilterControls/SortFilterControls'
 
 const Shop = () => {
-  const [filter, setFilter] = useState('');
-  const [sort, setSort] = useState('');
-  const [hoveredProductId, setHoveredProductId] = useState<number | null>(null);
-  const [page, setPage] = useState(1);
-  const productsPerPage = 15;
+  const [filter, setFilter] = useState('')
+  const [sort, setSort] = useState('')
+  const [hoveredProductId, setHoveredProductId] = useState<number | null>(null)
+  const [page, setPage] = useState(1)
+  const productsPerPage = 15
 
   useEffect(() => {
-    setPage(1); // Reset page to 1 when filter/sort is changed
-  }, [filter, sort]);
+    setPage(1)
+  }, [filter, sort])
 
   const { data: products, status: statusProducts } = useQuery(
     ['getAllProducts'],
     async () => {
-      return await fetchAllProductsUser();
+      return await fetchAllProductsUser()
     },
-  );
+  )
 
   const changePage = (newPage: number) => {
-    setPage(newPage);
-    window.scrollTo({ top: 0});
-  };
+    setPage(newPage)
+    window.scrollTo({ top: 0 })
+  }
 
   const filteredProducts = products
     ? products.filter((product) => {
-        const lowerCaseName = product.name.toLowerCase();
+        const lowerCaseName = product.name.toLowerCase()
         switch (filter) {
           case 'With pearls':
-            return lowerCaseName.includes('pearl');
+            return lowerCaseName.includes('pearl')
           case 'Without pearls':
-            return !lowerCaseName.includes('pearl');
+            return !lowerCaseName.includes('pearl')
           case 'Teas':
-            return lowerCaseName.includes('tea');
+            return lowerCaseName.includes('tea')
           case 'Smoothies':
-            return lowerCaseName.includes('smoothie');
+            return lowerCaseName.includes('smoothie')
           case 'Yogurts':
-            return lowerCaseName.includes('yogurt');
+            return lowerCaseName.includes('yogurt')
           case 'Fruit Drinks':
-            return lowerCaseName.includes('drink');
+            return lowerCaseName.includes('drink')
           case 'Dairy free':
-            return !/milk|smoothie|yogurt/.test(lowerCaseName);
+            return !/milk|smoothie|yogurt/.test(lowerCaseName)
           default:
-            return true;
+            return true
         }
       })
-    : [];
+    : []
 
-    const sortedProducts = [...filteredProducts].sort((a, b) => {
-      switch (sort) {
-        case 'Price (Low to High)':
-          return a.price - b.price;
-        case 'Price (High to Low)':
-          return b.price - a.price;
-        case 'Alphabetical (A to Z)':
-          return a.name.localeCompare(b.name);
-        default:
-          return 0;
-      }
-    });
-  
-    const totalPages = Math.ceil(sortedProducts.length / productsPerPage);
-  
-    const getPaginatedProducts = () => {
-      const start = (page - 1) * productsPerPage;
-      const end = start + productsPerPage;
-      return sortedProducts.slice(start, end);
-    };
-  
-    return (
-      <>
-        <LoadError status={statusProducts} />
-        {products && (
-          <div className="flex flex-col items-center" style={{ marginTop: '60px', marginBottom: '100px' }}>
-            <div>
-              <h1 className="text-4xl font-bold mt-2">Shop for Bubble Tea</h1>
-              <SortFilterControls filter={filter} sort={sort} setFilter={setFilter} setSort={setSort} />
-              <div className="grid grid-cols-3 gap-4">
-                {getPaginatedProducts().map((product) => (
-                  <div
+  const sortedProducts = [...filteredProducts].sort((a, b) => {
+    switch (sort) {
+      case 'Price (Low to High)':
+        return a.price - b.price
+      case 'Price (High to Low)':
+        return b.price - a.price
+      case 'Alphabetical (A to Z)':
+        return a.name.localeCompare(b.name)
+      default:
+        return 0
+    }
+  })
+
+  const totalPages = Math.ceil(sortedProducts.length / productsPerPage)
+
+  const getPaginatedProducts = () => {
+    const start = (page - 1) * productsPerPage
+    const end = start + productsPerPage
+    return sortedProducts.slice(start, end)
+  }
+
+  return (
+    <>
+      <LoadError status={statusProducts} />
+      {products && (
+        <div
+          className="flex flex-col items-center"
+          style={{ marginTop: '60px', marginBottom: '100px' }}
+        >
+          <div>
+            <h1 className="text-4xl font-bold mt-2">Shop for Bubble Tea</h1>
+            <SortFilterControls
+              filter={filter}
+              sort={sort}
+              setFilter={setFilter}
+              setSort={setSort}
+            />
+            <div className="grid grid-cols-3 gap-4">
+              {getPaginatedProducts().map((product) => (
+                <div
                   key={product.id}
                   className="border p-4 rounded-md flex flex-col justify-between"
                   style={{ width: '350px' }}
@@ -149,18 +157,40 @@ const Shop = () => {
                     </Link>
                   </div>
                 </div>
-                ))}
+              ))}
+            </div>
+            <div className="flex mt-4 justify-center">
+              <button
+                className={`${
+                  page === 1
+                    ? 'bg-gray-300 cursor-default'
+                    : 'bg-blue-500 hover:bg-blue-700'
+                } text-white font-bold py-2 px-4 mt-2 rounded-full w-128`}
+                disabled={page === 1}
+                onClick={() => changePage(page - 1)}
+              >
+                Prev Page
+              </button>
+              <div className="inline-block bg-gray-100 rounded-full px-3 py-2 text-l font-bold text-gray-700 mt-2 ml-2 mr-2 w-12 text-center">
+                {page}
               </div>
-              <div className="flex mt-4">
-                <button disabled={page === 1} onClick={() => changePage(page - 1)}>Previous Page</button>
-                <span className="mx-4">{page}</span>
-                <button disabled={page === totalPages} onClick={() => changePage(page + 1)}>Next Page</button>
-              </div>
+              <button
+                className={`${
+                  page === totalPages
+                    ? 'bg-gray-300 cursor-default'
+                    : 'bg-blue-500 hover:bg-blue-700'
+                } text-white font-bold py-2 px-4 mt-2 rounded-full w-128`}
+                disabled={page === totalPages}
+                onClick={() => changePage(page + 1)}
+              >
+                Next Page
+              </button>
             </div>
           </div>
-        )}
-      </>
-    );
-  };
-  
-  export default Shop;
+        </div>
+      )}
+    </>
+  )
+}
+
+export default Shop
