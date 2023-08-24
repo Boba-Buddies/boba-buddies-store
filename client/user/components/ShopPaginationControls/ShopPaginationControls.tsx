b19557e7cc0ff32d25f0c1531e6009f69dd4f295
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-08-25 09:29:57 +1200

    rename instances of ShopPaginationControls to ShopPaginationInterface

--- client/user/components/ShopPaginationControls/ShopPaginationControls.tsx
+++
@@ -1,7 +0,0 @@
-const ShopPaginationControls = () => {
-  return (
-    <div></div>
-  )
-}
-
-export default ShopPaginationControls
---
+++ client/user/components/ShopPaginationInterface/ShopPaginationInterface.tsx
@@ -0,0 +1,7 @@
+const ShopPaginationInterface = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default ShopPaginationInterface
