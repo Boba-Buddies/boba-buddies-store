ff556cb66579c2cd5cc17eaf597382783edd1ffe
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-08-29 10:54:09 +1200

    set up front end index with auth0

--- client/index.tsx
+++ client/index.tsx
@@ -79,8 +79,24 @@ function AppProvider() {
 document.addEventListener('DOMContentLoaded', () => {
   const queryClient = new QueryClient()
   createRoot(document.getElementById('app') as HTMLElement).render(
-    <QueryClientProvider client={queryClient}>
-      <AppProvider />
-    </QueryClientProvider>,
+    <Auth0Provider
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      domain={import.meta.env.VITE_AUTH0_DOMAIN as string}
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID as string}
+      cacheLocation="localstorage"
+      authorizationParams={{
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        audience: import.meta.env.VITE_AUTH0_AUDIENCE as string,
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <QueryClientProvider client={queryClient}>
+        <AppProvider />
+      </QueryClientProvider>
+    </Auth0Provider>,
   )
 })
