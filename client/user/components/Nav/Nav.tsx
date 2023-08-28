bd867e904a21e0a81c2b8710a43759ff56a57770
Rigelle Cruz <rigellecruz@gmail.com>
2023-08-28 12:17:56 +1200

    progress on nav bar styling

--- client/user/components/Nav/Nav.tsx
+++ client/user/components/Nav/Nav.tsx
@@ -8,44 +8,47 @@ const Nav = () => {
     navigate(link)
   }
   return (
-    <div className="bg-black h-16 flex justify-center items-center">
-      <div className="flex gap-6 text-white text-sm">
+    <div className="bg-black h-16 flex justify-between items-center">
+      <div className="flex align-center gap-4 text-white">
         <button
-          className="hover:text-rose-600 transition-colors duration-300"
+          className="hover:text-purple-700 transition-colors duration-300"
           onClick={() => goTo('/')}
         >
           Home
         </button>
         <button
-          className="hover:text-rose-600 transition-colors duration-300"
+          className="hover:text-purple-700 transition-colors duration-300"
           onClick={() => goTo('shop')}
         >
           Shop
         </button>
         <button
-          className="hover:text-rose-600 transition-colors duration-300"
+          className="hover:text-purple-700 transition-colors duration-300"
           onClick={() => goTo('cart')}
         >
           Cart
         </button>
-        <div className="group relative">
-          <button
-            className="hover:text-rose-600 transition-colors duration-300"
-            onClick={() => goTo('wishlist')}
-          >
-            <FontAwesomeIcon icon={faHeart} className="text-xl" />
-          </button>
-          <span className="absolute left-[-50%] bottom-[-110%] bg-gray-500 text-white px-2 py-1 rounded shadow text-xs opacity-0 pointer-events-none transition-opacity duration-300 group-hover:opacity-100">
-            Wishlist
-          </span>
-        </div>
+
         <button
-          className="hover:text-rose-600 transition-colors duration-300"
+          className="hover:text-purple-700 transition-colors duration-300"
           onClick={() => goTo('profile')}
         >
           Account
         </button>
-        <button className="hover:text-rose-600 transition-colors duration-300">
+        <div className="group relative">
+          <button
+            className="hover:text-purple-700 transition-colors duration-300"
+            onClick={() => goTo('wishlist')}
+          >
+            <FontAwesomeIcon icon={faHeart} className="text-xl" />
+          </button>
+          <span className="absolute left-1/2 -bottom-6 bg-gray-500 text-white px-2 py-1 rounded shadow text-xs opacity-0 pointer-events-none transition-opacity duration-300 group-hover:opacity-100">
+            Wishlist
+          </span>
+        </div>
+      </div>
+      <div className="p-4" style={{ background: 'blue' }}>
+        <button className="text-white hover:text-purple-700 transition-colors duration-300">
           Login
         </button>
       </div>
