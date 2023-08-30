b5116197445858329c0803398d047bd505d7a1c6
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-08-31 11:29:59 +1200

    configure products apis to work with auth0 tokens

--- client/apis/products.ts
+++ client/apis/products.ts
@@ -1,84 +1,77 @@
-import request from 'superagent'
-import { UserProduct, UpsertProduct, AdminProduct } from '../../models/Products'
+import request from 'superagent';
+import { UserProduct, UpsertProduct, AdminProduct } from '../../models/Products';
 
-const baseUrl = '/api/v1/products'
-
-//!ADMIN ONLY FUNCTION
-export async function fetchProductByIdAdmin(id: number) {
-  try {
-    const response = await request.get(`${baseUrl}/admin/${id}`)
-    return response.body as AdminProduct
-  } catch (error) {
-    console.error('Error fetching product by ID:', (error as Error).message)
-    throw { error: (error as Error).message }
-  }
-}
-
-export async function fetchProductByIdUser(id: number) {
-  try {
-    const response = await request.get(`${baseUrl}/${id}`)
-    return response.body as UserProduct
-  } catch (error) {
-    console.error('Error fetching product by ID:', (error as Error).message)
-    throw { error: (error as Error).message }
-  }
-}
+const baseUrl = '/api/v1/products';
 
 //!ADMIN ONLY FUNCTION
-export async function fetchAllProductsAdmin() {
+export async function fetchProductByIdAdmin(id: number, token: string) {
   try {
-    const response = await request.get(`${baseUrl}/admin`)
-    return response.body as AdminProduct[]
+    const response = await request.get(`${baseUrl}/admin/${id}`).set('Authorization', `Bearer ${token}`);
+    return response.body as AdminProduct;
   } catch (error) {
-    console.error('Error fetching all products:', (error as Error).message)
-    throw { error: (error as Error).message }
+    console.error('Error fetching product by ID:', (error as Error).message);
+    throw { error: (error as Error).message };
   }
 }
 
-
-export async function fetchAllProductsUser() {
+export async function fetchProductByIdUser(id: number, token: string) {
   try {
-    const response = await request.get(baseUrl)
-    return response.body as UserProduct[]
+    const response = await request.get(`${baseUrl}/${id}`).set('Authorization', `Bearer ${token}`);
+    return response.body as UserProduct;
   } catch (error) {
-    console.error('Error fetching all products:', (error as Error).message)
-    throw { error: (error as Error).message }
+    console.error('Error fetching product by ID:', (error as Error).message);
+    throw { error: (error as Error).message };
   }
 }
 
 //!ADMIN ONLY FUNCTION
-export async function fetchAmountOfProductsBelowStockLevel(maxStock: number) {
+export async function fetchAllProductsAdmin(token: string) {
   try {
-    const response = await request.get(`${baseUrl}/lowstock/${maxStock}`)
-    return response.body
+    const response = await request.get(`${baseUrl}/admin`).set('Authorization', `Bearer ${token}`);
+    return response.body as AdminProduct[];
   } catch (error) {
-    console.error(
-      'Error fetching products below stock level:',
-      (error as Error).message,
-    )
-    return { error: (error as Error).message }
+    console.error('Error fetching all products:', (error as Error).message);
+    throw { error: (error as Error).message };
+  }
+}
+
+export async function fetchAllProductsUser(token: string) {
+  try {
+    const response = await request.get(baseUrl).set('Authorization', `Bearer ${token}`);
+    return response.body as UserProduct[];
+  } catch (error) {
+    console.error('Error fetching all products:', (error as Error).message);
+    throw { error: (error as Error).message };
   }
 }
 
 //!ADMIN ONLY FUNCTION
-export async function modifyProductById(
-  id: number,
-  updatedProduct: UpsertProduct,
-) {
+export async function fetchAmountOfProductsBelowStockLevel(maxStock: number, token: string) {
   try {
-    await request.patch(`${baseUrl}/${id}`).send(updatedProduct)
+    const response = await request.get(`${baseUrl}/lowstock/${maxStock}`).set('Authorization', `Bearer ${token}`);
+    return response.body;
   } catch (error) {
-    console.error('Error modifying product by ID:', (error as Error).message)
-    return { error: (error as Error).message }
+    console.error('Error fetching products below stock level:', (error as Error).message);
+    throw { error: (error as Error).message };
   }
 }
 
 //!ADMIN ONLY FUNCTION
-export async function createProduct(newProduct: UpsertProduct) {
+export async function modifyProductById(id: number, updatedProduct: UpsertProduct, token: string) {
   try {
-    await request.post(baseUrl).send(newProduct)
+    await request.patch(`${baseUrl}/${id}`).send(updatedProduct).set('Authorization', `Bearer ${token}`);
   } catch (error) {
-    console.error('Error creating product:', (error as Error).message)
-    return { error: (error as Error).message }
+    console.error('Error modifying product by ID:', (error as Error).message);
+    throw { error: (error as Error).message };
+  }
+}
+
+//!ADMIN ONLY FUNCTION
+export async function createProduct(newProduct: UpsertProduct, token: string) {
+  try {
+    await request.post(baseUrl).send(newProduct).set('Authorization', `Bearer ${token}`);
+  } catch (error) {
+    console.error('Error creating product:', (error as Error).message);
+    throw { error: (error as Error).message };
   }
 }
