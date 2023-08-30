f8804fed929419d0b5dcb84cda3db0254b7bbd2e
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-08-31 09:54:21 +1200

    make shipping options routes work with auth0

--- server/routes/shippingOptions.ts
+++ server/routes/shippingOptions.ts
@@ -5,8 +5,15 @@ import { validateAccessToken } from '../auth0'
 const router = Router()
 
 
-// GET /api/v1/shippings/
+// GET /api/v1/shipping-options/
 router.get('/', validateAccessToken, async (req, res) => {
+  const userId = req.auth?.payload.sub
+
+  if (!userId) {
+    res.status(400).json({ message: 'Please provide an id' })
+    return
+  }
+
   try {
     const shippingOptions = await db.getAllShippingOptions()
     res.status(200).json({ shippingOptions })
