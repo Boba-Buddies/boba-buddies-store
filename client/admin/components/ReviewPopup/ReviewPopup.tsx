fa62ee3a7e42267bd6ce85b4b4e2ad5c25f82922
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-09-08 10:44:46 +1200

    modify ReviewPopup to make it appear 'on top' of the admin reivews page

--- client/admin/components/ReviewPopup/ReviewPopup.tsx
+++ client/admin/components/ReviewPopup/ReviewPopup.tsx
@@ -1,4 +1,6 @@
 import { Review } from '../../../../models/Reviews'
+import StarRating from '../../../user/components/StarRating/StarRating'
+import { formatDateToDDMMYYYY } from '../../../utils/formatDate/formatDate'
 
 interface ReviewPopupProps {
   review: Review
@@ -8,20 +10,68 @@ interface ReviewPopupProps {
 
 const ReviewPopup = ({ review, onClose, onToggle }: ReviewPopupProps) => {
   return (
-    <div className="review-popup">
-      <button onClick={onClose}>Back to reviews</button>
-      <h2>{review.reviewerUserName}</h2>
-      <p>{review.reviewCreatedAt}</p>
-      <img src={review.productImage} alt={review.productName} />
-      <h3>{review.productName}</h3>
-      <p>Rating: {review.reviewRating}</p>
-      <p>{review.reviewDescription}</p>
-      <button
-        style={{ backgroundColor: review.reviewIsEnabled ? 'green' : 'red' }}
-        onClick={() => onToggle(review.reviewId, !review.reviewIsEnabled)}
+    <div
+      style={{
+        backgroundColor: 'rgba(0, 0, 0, 0.7)',
+        position: 'fixed',
+        top: '0',
+        left: '0',
+        right: '0',
+        bottom: '0',
+        zIndex: 1000,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: 'white',
+          padding: '20px',
+          borderRadius: '10px',
+          zIndex: 1001,
+          width: '80%',
+          maxWidth: '500px',
+          minHeight: '400px',
+        }}
+        className = "flex flex-col justify-between"
       >
-        {review.reviewIsEnabled ? 'Enabled' : 'Disabled'}
-      </button>
+        <div>
+        <button style = {{marginBottom : '20px'}}onClick={onClose}>Back to reviews</button>
+          <div className="flex justify-between">
+            <h2>{review.reviewerUserName}</h2>
+            <p>{formatDateToDDMMYYYY(review.reviewCreatedAt)}</p>
+          </div>
+          <div className="flex mt-8 items-center">
+            <img
+              style={{ maxWidth: '150px' }}
+              src={review.productImage}
+              alt={review.productName}
+            />
+            <h2>{review.productName}</h2>
+          </div>
+        </div>
+        <div>
+          <div className="flex gap-4 mt-4">
+            <p>Rating:</p>
+            <StarRating rating={review.reviewRating} size={1} />
+            <p>({review.reviewRating})</p>
+          </div>
+          <h2>Description:</h2>
+          <p>{review.reviewDescription}</p>
+          
+        </div>
+        <button
+            style={{
+              backgroundColor: review.reviewIsEnabled ? 'green' : 'red',
+              marginTop : '30px',
+              width: '80px'
+            }}
+            onClick={() => onToggle(review.reviewId, !review.reviewIsEnabled)}
+          >
+            {review.reviewIsEnabled ? 'Enabled' : 'Disabled'}
+          </button>
+      </div>
     </div>
   )
 }
