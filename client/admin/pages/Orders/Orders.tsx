31dcc99bbcd60f6bcfc170d157fdca3b6deb3275
Rigelle Cruz <rigellecruz@gmail.com>
2023-09-08 10:28:37 +1200

    set up react-query in orders page

--- client/admin/pages/Orders/Orders.tsx
+++ client/admin/pages/Orders/Orders.tsx
@@ -1,3 +1,10 @@
+import { useAuth0 } from '@auth0/auth0-react'
+import { useQuery, useQueryClient } from 'react-query'
+
 export const Orders = () => {
+  const { getAccessTokenSilently } = useAuth0()
+  const queryClient = useQueryClient()
+  
+
   return <div>Orders</div>
 }
