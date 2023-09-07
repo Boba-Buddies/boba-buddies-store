8c32f86d1631a3d1eeedb64f6180d7dfb95fdd3d
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-09-08 11:46:49 +1200

    add cursor-pointer when user hovers over a review

--- client/admin/pages/Reviews/Reviews.tsx
+++ client/admin/pages/Reviews/Reviews.tsx
@@ -202,7 +202,7 @@ const Reviews = () => {
                 {currentReviews.map((review) => (
                   <div
                     key={review.id}
-                    className="divRow border-b border-gray-200 hover:bg-gray-100"
+                    className="divRow border-b border-gray-200 hover:bg-gray-100 cursor-pointer"
                     onClick={() => fetchAndShowReviewDetails(review.id)}
                   >
                     <div
