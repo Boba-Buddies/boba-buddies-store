ee04ce2a00600e92ee40a83f1d2e00189f0bf7e3
Rigelle Cruz <rigellecruz@gmail.com>
2023-09-12 11:54:26 +1200

    add some scroll feature to the order popup

--- client/admin/pages/Orders/OrderPopup.tsx
+++ client/admin/pages/Orders/OrderPopup.tsx
@@ -52,54 +52,42 @@ const OrderPopup = ({ orderId, order, closeOrderPopup }: OrderPopupProps) => {
             ref={popupRef}
             className="bg-white p-8 rounded-lg w-96 max-w-full max-h-full overflow-y-auto shadow-xl"
           >
-            <div className="flex justify-between items-center">
+            <div className="mb-4">
               <h2 className="text-xl font-semibold">Order Details</h2>
+            </div>
+            <div className="mb-4">
+              <h3 className="text-lg font-semibold">User Information:</h3>
+              <p>
+                User Name: {order.userFirstName} {order.userLastName}
+              </p>
+              <p>User Email: {order.userEmail}</p>
+              <p>User Phone Number: {order.userPhoneNumber}</p>
+              <p>User Address: {order.userAddress}</p>
+              <p>User City: {order.userCity}</p>
+              <p>User Country: {order.userCountry}</p>
+              <p>User Zip Code: {order.userZipCode}</p>
+            </div>
+            <div className="mb-4">
+              <h3 className="text-lg font-semibold">Order Items:</h3>
+              <ul>
+                {order.orderItems.map((item) => (
+                  <li key={item.productSale} className="mb-2">
+                    <p>Product Name: {item.productName}</p>
+                    <p>Product Sale: {item.productSale}</p>
+                    <p>Product Image: {item.productImage}</p>
+                    <p>Item Quantity: {item.itemQuantity}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <hr className="border-t border-gray-300" />
+            <div className="mt-4 text-right">
               <button
                 onClick={closeOrderPopup}
                 className="px-2 py-1 text-white bg-blue-600 rounded hover:bg-blue-700"
               >
                 Back to orders
               </button>
-            </div>
-            <hr className="my-4 border-t border-gray-300" />
-            <div>
-              <div className="mb-4">
-                <h3 className="text-lg font-semibold">Order ID:</h3>
-                <p>{order.orderId}</p>
-              </div>
-              <div className="mb-4">
-                <h3 className="text-lg font-semibold">User Information:</h3>
-                <p>
-                  User Name: {order.userFirstName} {order.userLastName}
-                </p>
-                <p>User Email: {order.userEmail}</p>
-                <p>User Phone Number: {order.userPhoneNumber}</p>
-                <p>User Address: {order.userAddress}</p>
-                <p>User City: {order.userCity}</p>
-                <p>User Country: {order.userCountry}</p>
-                <p>User Zip Code: {order.userZipCode}</p>
-              </div>
-              <div className="mb-4">
-                <h3 className="text-lg font-semibold">Order Information:</h3>
-                <p>Total Sale: {order.totalSale}</p>
-                <p>Amount Of Items: {order.amountOfItems}</p>
-                <p>Order Date: {order.orderDate}</p>
-                <p>Shipping Type: {order.shippingType}</p>
-                <p>Shipping Price: {order.shippingPrice}</p>
-              </div>
-              <div>
-                <h3 className="text-lg font-semibold">Order Items:</h3>
-                <ul>
-                  {order.orderItems.map((item) => (
-                    <li key={item.productSale}>
-                      <p>Product Name: {item.productName}</p>
-                      <p>Product Sale: {item.productSale}</p>
-                      <p>Product Image: {item.productImage}</p>
-                      <p>Item Quantity: {item.itemQuantity}</p>
-                    </li>
-                  ))}
-                </ul>
-              </div>
             </div>
           </div>
         </div>
