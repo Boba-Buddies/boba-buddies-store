f3d127e711d41ae54104ee4666e9eb106fce0feb
Zhang Jane <jiejanezhang2012@gmail.com>
2023-09-12 00:04:51 +1200

    styling

--- client/admin/components/EditProduct/EditProduct.tsx
+++ client/admin/components/EditProduct/EditProduct.tsx
@@ -79,9 +79,9 @@ function EditProduct({
 
   return (
     <form>
-      <div className="flex items-center max-w-5xl" style={{ padding: '10px' }}>
-        <div className="w-1/2">
-          <img src={product.image} alt={product.name} className="w-full" />
+      <div className="flex flex-col max-w-5xl" style={{ padding: '10px' }}>
+        <div className="flex flex-row items-center mb-16">
+          <img src={product.image} alt={product.name} className="w-1/4" />
 
           {/* fix upload image */}
           <label>
@@ -90,36 +90,48 @@ function EditProduct({
           </label>
         </div>
 
+        <div className='w-4/5'>
 
-        <div className="w-1/2 ml-4">
-          <div className="flex items-center">
+          <div className='flex flex-row justify-between'>
+            <div className='ml-6'>
+              <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded" onClick={handleClick} name='isEnabled' disabled={!editedProduct.isEnabled} type='button'>Enabled</button>
+              <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded" onClick={handleClick} name='isEnabled' disabled={editedProduct.isEnabled} type='button'>Disabled</button>
+            </div>
+
+            <button
+              className="font-bold py-2 px-4 rounded bg-black text-white"
+              type="button"
+              onClick={saveChanges}
+            >
+              SAVE CHANGES
+            </button>
+          </div>
+
+
+          <div className='mt-6 ml-6'>
             <input className="text-3xl font-bold" onChange={handleChange} name='name' type='text' value={editedProduct.name}></input>
           </div>
 
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleClick} name='isEnabled' disabled={!editedProduct.isEnabled} type='button'>Enabled</button>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleClick} name='isEnabled' disabled={editedProduct.isEnabled} type='button'>Disabled</button>
+          <div className='flex flex-row justify-between mt-6 ml-6'>
+            <div className=" flex items-center text-3xl font-bold mr-6">
+              <label className='w-max' htmlFor="price">Price: $</label>
+              <input className='border ml-1' onChange={handleChange} name='price' type='number' value={editedProduct.price}></input>
+            </div>
 
-          <div className="flex items-center">
-            <span className="text-3xl font-bold">$</span>
-            <input className="text-3xl font-bold" onChange={handleChange} name='price' type='number' value={editedProduct.price}></input>
+            <div className="flex items-center text-3xl font-bold">
+              <label htmlFor="stock"> Stock: </label>
+              <input className='border ml-1' onChange={handleChange} name='stock' type='number' value={editedProduct.stock}></input>
+            </div>
           </div>
 
-          <div className="flex items-center">
-            <input className="text-3xl font-bold" onChange={handleChange} name='stock' type='number' value={editedProduct.stock}></input>
+          <div className='mt-6'>
+            <label className='text-3xl font-bold ml-6' htmlFor="description">Description</label>
+            <textarea rows={10} cols={100} className="border text-xl ml-6 w-max" onChange={handleDescriptionChange} name='description' value={editedProduct.description}></textarea>
           </div>
 
-          <textarea rows={10} className="text-xl" onChange={handleDescriptionChange} name='description' value={editedProduct.description}></textarea>
-
-          {ProductMutation.isError ? <p>Error adding product to cart</p> : null}
         </div>
+
       </div>
-      <button
-        className="ml-2 mr-2 mt-6 rounded-md bg-black text-white p-4 w-full text-2xl font-bold"
-        type="button"
-        onClick={saveChanges}
-      >
-        SAVE CHANGES
-      </button>
     </form>
   )
 }
