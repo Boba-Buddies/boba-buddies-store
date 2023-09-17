f5020f11a2f7e0fc52fdc86f7260df57b7675c05
Rigelle Cruz <rigellecruz@gmail.com>
2023-09-18 11:28:13 +1200

    update index.tsx - rename Orders to AllOrders

--- client/index.tsx
+++ client/index.tsx
@@ -25,7 +25,7 @@ import {
 } from './user/pages/index'
 import { Reviews, Dashboard } from './admin/pages/'
 import Redirect from './user/pages/Redirect/Redirect'
-import Orders from './admin/pages/Orders/Orders'
+import AllOrders from './admin/pages/Orders/AllOrders'
 import AdiminComponent from './UI/AdiminComponent'
 import ProtectedComponent from './UI/ProtectedComponent'
 
@@ -75,7 +75,7 @@ export const routes = createRoutesFromElements(
 
     <Route
       path="admin/orders"
-      element={<AdiminComponent component={Orders} />}
+      element={<AdiminComponent component={AllOrders} />}
     />
   </Route>,
 )
