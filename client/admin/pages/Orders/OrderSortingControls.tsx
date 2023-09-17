a659b5ade3eb19461ef104e9cffb36bb96822a4b
Rigelle Cruz <rigellecruz@gmail.com>
2023-09-18 11:12:15 +1200

    Add pagination to OrderSortingControls.tsx component

--- client/admin/pages/Orders/OrderSortingControls.tsx
+++ client/admin/pages/Orders/OrderSortingControls.tsx
@@ -3,9 +3,9 @@ interface OrderSortingControlsProps {
   setSearch: React.Dispatch<React.SetStateAction<string>>
   sort: string
   setSort: React.Dispatch<React.SetStateAction<string>>
-  currentPage: number 
-  totalPages: number 
-  setCurrentPage: React.Dispatch<React.SetStateAction<number>> // 
+  currentPage: number
+  totalPages: number
+  setCurrentPage: React.Dispatch<React.SetStateAction<number>>
 }
 
 function OrderSortingControls({
@@ -13,6 +13,9 @@ function OrderSortingControls({
   setSearch,
   sort,
   setSort,
+  currentPage,
+  totalPages,
+  setCurrentPage,
 }: OrderSortingControlsProps) {
   return (
     <div className="border p-2 rounded flex justify-between items-center">
@@ -36,6 +39,32 @@ function OrderSortingControls({
           <option value="newest">Newest first</option>
           <option value="oldest">Oldest first</option>
         </select>
+      </div>
+
+      {/* PAGINATION */}
+      <div className="flex justify-between items-center">
+        <button
+          className={`${
+            currentPage === 1
+              ? 'bg-gray-300 cursor-default'
+              : 'bg-blue-500 hover:bg-blue-700'
+          } text-white font-bold py-2 px-4 rounded`}
+          disabled={currentPage === 1}
+          onClick={() => setCurrentPage(currentPage - 1)}
+        >
+          {'<'}
+        </button>
+        <button
+          className={`${
+            currentPage === totalPages
+              ? 'bg-gray-300 cursor-default'
+              : 'bg-blue-500 hover:bg-blue-700'
+          } text-white font-bold py-2 px-4 rounded ml-2`}
+          disabled={currentPage === totalPages}
+          onClick={() => setCurrentPage(currentPage + 1)}
+        >
+          {'>'}
+        </button>
       </div>
     </div>
   )
