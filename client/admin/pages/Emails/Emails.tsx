526b237fea90e1007f2861b3dae882593280f4f1
Sarah Chung <sarahchungwu@gmail.com>
2023-09-18 11:21:21 +1200

    the emails react query worked

--- client/admin/pages/Emails/Emails.tsx
+++ client/admin/pages/Emails/Emails.tsx
@@ -1,9 +1,25 @@
+import { useAuth0 } from '@auth0/auth0-react'
+import { useQuery } from 'react-query'
+import { fetchAllEmails } from '../../../apis/emails'
+import LoadError from '../../../user/components/LoadError/LoadError'
+
 const Emails = () => {
+  const { getAccessTokenSilently } = useAuth0()
+  const { data: emails, status: emailStatus } = useQuery(
+    ['getReviews'],
+    async () => {
+      const token = await getAccessTokenSilently()
+      return await fetchAllEmails(token)
+    },
+  )
+  console.log(emails)
+
   return (
     <>
-      {/* FilterBox */}
+      <LoadError status={emailStatus} />
+      {/* SortingControl */}
       <div>
-        <div>This the filter box</div>
+        <div>This a sortingControl Email box</div>
       </div>
       <div>Email</div>
     </>
