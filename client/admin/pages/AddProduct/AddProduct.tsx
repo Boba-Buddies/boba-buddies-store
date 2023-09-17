f8bc8853309b791d30e4e9f39f3fad09d22f17a4
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-09-18 11:37:52 +1200

    Add styling for AddProduct

--- client/admin/pages/AddProduct/AddProduct.tsx
+++ client/admin/pages/AddProduct/AddProduct.tsx
@@ -48,12 +48,13 @@ const AddProduct = () => {
   const handleChange = (
     event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
   ) => {
-    const { name, value } = event.target;
-  
-    const finalValue = (name === 'price' || name === 'stock') ? parseFloat(value) : value;
-  
-    setNewProduct((prevProduct) => ({ ...prevProduct, [name]: finalValue }));
-  };
+    const { name, value } = event.target
+
+    const finalValue =
+      name === 'price' || name === 'stock' ? parseFloat(value) : value
+
+    setNewProduct((prevProduct) => ({ ...prevProduct, [name]: finalValue }))
+  }
 
   const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { checked } = event.target
@@ -67,78 +68,94 @@ const AddProduct = () => {
   return (
     <>
       <LoadError status={addProductMutation.status} />
-      <div>
-        <h1>Add Product</h1>
+      <div className="container mx-auto mt-12" style={{maxWidth : '500px'}}>
+        <h1 className="text-3xl font-semibold mb-4">Add Product</h1>
         <form>
-          <div>
-            <label>
-              Image URL:
-              <input
-                type="text"
-                name="image"
-                value={newProduct.image}
-                onChange={handleChange}
-              />
-            </label>
-          </div>
-          <div>
-            <img src={newProduct.image} alt="Product preview" width={100} />
-          </div>
-          <div>
-            <label>
-              Enabled:
-              <input
-                type="checkbox"
-                checked={newProduct.isEnabled}
-                onChange={handleCheckboxChange}
-              />
-            </label>
-          </div>
-          <div>
-            <label>
+        <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
               Name:
-              <input
-                type="text"
-                name="name"
-                value={newProduct.name}
-                onChange={handleChange}
-              />
             </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="text"
+              name="name"
+              value={newProduct.name}
+              onChange={handleChange}
+            />
           </div>
-          <div>
-            <label>
-              Price:
-              <input
-                type="number"
-                name="price"
-                value={newProduct.price}
-                onChange={handleChange}
-              />
-            </label>
-          </div>
-          <div>
-            <label>
-              Stock:
-              <input
-                type="number"
-                name="stock"
-                value={newProduct.stock}
-                onChange={handleChange}
-              />
-            </label>
-          </div>
-          <div>
-            <label>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
               Description:
-              <textarea
-                name="description"
-                value={newProduct.description}
-                onChange={handleChange}
-              ></textarea>
             </label>
+            <textarea
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              name="description"
+              value={newProduct.description}
+              onChange={handleChange}
+            ></textarea>
           </div>
-          <div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Price:
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="number"
+              name="price"
+              value={newProduct.price}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Stock:
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="number"
+              name="stock"
+              value={newProduct.stock}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Image URL:
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="text"
+              name="image"
+              value={newProduct.image}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-4">
+            <img
+              src={newProduct.image}
+              alt="Product preview"
+              className="w-24"
+            />
+          </div>
+          <div className="mb-4 flex">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Enabled:
+            </label>
+            <input
+              className="mr-2 leading-tight"
+              type="checkbox"
+              checked={newProduct.isEnabled}
+              onChange={handleCheckboxChange}
+            />
+          </div>
+          
+          
+          
+          <div className="mb-4">
             <button
+              className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${
+                !isFormComplete && 'opacity-50 cursor-not-allowed'
+              }`}
               type="button"
               disabled={!isFormComplete}
               onClick={handleSubmit}
