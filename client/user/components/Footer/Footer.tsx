91ac80cc2dc2e24df1046f23629018a2fbbc1617
Rigelle Cruz <rigellecruz@gmail.com>
2023-09-19 10:50:41 +1200

    create handleLoginClick for footer login

--- client/user/components/Footer/Footer.tsx
+++ client/user/components/Footer/Footer.tsx
@@ -1,5 +1,35 @@
+import { useAuth0 } from '@auth0/auth0-react'
+
 const Footer = () => {
-  return <div>This is the footer</div>
+
+  const { loginWithRedirect } = useAuth0()
+
+  const handleLoginClick = () => {
+    loginWithRedirect({
+      authorizationParams: {
+        redirect_uri: `${window.location.origin}/redirect`,
+      },
+    })
+  }
+
+  return (
+    <footer className="bg-black text-white py-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div>&copy; {new Date().getFullYear()} Boba Buddies</div>
+        <div className="space-x-4">
+          <a href="/shop" className="hover:text-purple-700">
+            Shop
+          </a>
+          <a href="/contact" className="hover:text-purple-700">
+            Contact
+          </a>
+          <a href="/contact" className="hover:text-purple-700">
+            login
+          </a>
+        </div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
