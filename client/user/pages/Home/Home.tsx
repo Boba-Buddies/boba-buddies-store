f07cd055ddda6dc12f23dbe4caa8742af24d73ce
Rigelle Cruz <rigellecruz@gmail.com>
2023-09-19 11:28:41 +1200

    progress on styling the home page

--- client/user/pages/Home/Home.tsx
+++ client/user/pages/Home/Home.tsx
@@ -6,37 +6,36 @@ const Home = () => {
     navigate(link)
   }
   return (
-    <div className="bg-white text-black flex flex-col items-center font-sans py-8">
+    <div
+      className="items-center font-sans py-8"
+      style={{ background: '#FFC5C7' }}
+    >
       <div className="max-w-screen-xl w-full p-8 flex flex-col items-center mb-60">
         {/* Welcome Card */}
-        <div className="bg-white border shadow-md rounded-lg p-10 flex flex-col items-center mb-36 w-4/5">
-          <h1 className="text-6xl font-bold my-20  text-center w-3/4">
-            Welcome to the Boba Buddies Store
-          </h1>
-          <button
-            onClick={() => goTo('shop')}
-            className="bg-black text-white px-6 py-2 rounded-lg hover:bg-gray-700 transition-all"
-          >
-            Buy some tea!
-          </button>
-        </div>
-
-        {/* Contact and Admin Login Card */}
-        <div className="bg-white border shadow-md rounded-lg py-8 px-24 flex flex-row justify-between items-center w-4/5">
-          <div className="w-1/4 ml-7">
+        <div className=" p-10 flex flex-row items-center mb-36 w-4/5">
+          <img
+            src="/images/home-tea.svg"
+            alt="Boba Buddies Logo"
+            className="max-w-full h-auto"
+          />
+          <div className="flex flex-col text-white">
             <img
-              src="/images/bobaLogo.png"
+              src="/images/home-title.svg"
               alt="Boba Buddies Logo"
               className="max-w-full h-auto"
             />
-          </div>
+            <p>
+              Dive into our colorful world of delicious flavors, hand-shaken to
+              perfection. Join the fun, and become a boba buddy today!
+            </p>
 
-          <button
-            className="text-white bg-black px-4 py-2 rounded-lg hover:bg-gray-700 transition-all"
-            onClick={() => goTo('admin')}
-          >
-            Admin login
-          </button>
+            <button
+              onClick={() => goTo('shop')}
+              className="border text-white px-6 py-2 rounded-lg hover:bg-white transition-all"
+            >
+              Shop
+            </button>
+          </div>
         </div>
       </div>
     </div>
