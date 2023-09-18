aa50d2ea6fd1a4521a8c217d33e46d218375797b
Sarah Chung <sarahchungwu@gmail.com>
2023-09-18 17:58:37 +1200

    created the table title for the table

---
+++ client/admin/components/Emails/EmailsColumnTitles.tsx
@@ -0,0 +1,17 @@
+const EmailsColumnTitles = () => {
+  return (
+    <div className="divRow bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+      <div className="divCell py-3 px-8" style={{ minWidth: '50px' }}>
+        UserName
+      </div>
+      <div className="divCell py-3 px-8" style={{ minWidth: '50px' }}>
+        Title
+      </div>
+      <div className="divCell py-3 px-8" style={{ minWidth: '50px' }}>
+        Date Created
+      </div>
+    </div>
+  )
+}
+
+export default EmailsColumnTitles
--- client/admin/pages/Emails/Emails.tsx
+++ client/admin/pages/Emails/Emails.tsx
@@ -2,6 +2,7 @@ import { useAuth0 } from '@auth0/auth0-react'
 import { useQuery } from 'react-query'
 import { fetchAllEmails } from '../../../apis/emails'
 import LoadError from '../../../user/components/LoadError/LoadError'
+import EmailsColumnTitles from '../../components/Emails/EmailsColumnTitles'
 
 const Emails = () => {
   const { getAccessTokenSilently } = useAuth0()
@@ -18,10 +19,13 @@ const Emails = () => {
     <>
       <LoadError status={emailStatus} />
       {/* SortingControl */}
-      <div>
-        <div>This a sortingControl Email box</div>
+      <div className="flex justify-center">
+        <div className="p-4 w-full lg:w-11/12">
+          <div className="divTable w-full bg-white mt-4 border border-gray-300">
+            <EmailsColumnTitles />
+          </div>
+        </div>
       </div>
-      <div>Email</div>
     </>
   )
 }
