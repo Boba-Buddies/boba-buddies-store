6f6d28c938e1618dbed516d14d48cc4a080fd1d6
Rigelle Cruz <rigellecruz@gmail.com>
2023-09-19 14:28:53 +1200

    modify homepage styling

--- client/user/pages/Home/Home.tsx
+++ client/user/pages/Home/Home.tsx
@@ -6,20 +6,19 @@ const Home = () => {
     navigate(link)
   }
   return (
-    <div style={{ background: '#FFC5C7' }}>
+    <div className="min-h-screen" style={{ background: '#FFC5C7' }}>
       <div
-        className=" flex items-center justify-center"
+        className="flex items-center justify-center min-h-screen"
         style={{
-          paddingBottom: '36px',
           backgroundImage: `url('/images/ellipse1.svg'), url('/images/ellipse2.svg')`,
           backgroundPosition: 'bottom left, top right',
           backgroundRepeat: 'no-repeat',
         }}
       >
-        <div className=" flex items-center justify-center">
+        <div className="flex items-center justify-center">
           <div className="max-w-screen-xl flex flex-col items-center">
             {/* Welcome Card */}
-            <div className=" p-12 space-y-12 flex flex-col md:flex-row items-center w-4/5 md">
+            <div className="p-12 space-y-12 flex flex-col md:flex-row items-center w-4/5 md">
               <img
                 src="/images/home-tea.svg"
                 alt="Boba Buddies Logo"
@@ -27,7 +26,7 @@ const Home = () => {
                 style={{ width: '65%', maxWidth: '100%' }}
               />
 
-              <div className=" flex flex-col text-white text-xl space-y-8 p-10">
+              <div className="flex flex-col text-white text-xl space-y-8 p-10">
                 <img
                   src="/images/home-title.svg"
                   alt="Boba Buddies Logo"
@@ -41,7 +40,7 @@ const Home = () => {
 
                 <button
                   onClick={() => goTo('shop')}
-                  className="w-1/2 border border-white border-2  text-white px-6 py-2 rounded-2xl hover:bg-white hover:text-rose-300 transition-all"
+                  className="w-1/2 border border-white border-2 text-white px-6 py-2 rounded-2xl hover:bg-white hover:text-rose-300 transition-all"
                 >
                   Shop
                 </button>
