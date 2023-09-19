bc0788098b521b4255f915ed577187300c31296f
Rigelle Cruz <rigellecruz@gmail.com>
2023-09-19 14:55:43 +1200

    add map to contact page

--- client/user/pages/Contact/Contact.tsx
+++ client/user/pages/Contact/Contact.tsx
@@ -55,14 +55,14 @@ function Contact() {
   }, [alertMessage])
 
   return (
-    <div className="mx-auto max-w-4xl p-8">
-      <div className="text-2xl text-center font-bold">Contact</div>
+    <div className="mx-auto max-w-4xl p-8 min-h-screen">
+      <div className="text-3xl text-center font-bold mb-6">Contact Us</div>
       {alertMessage && (
-        <div className="bg-green-200 text-green-800 p-2 mt-2 rounded">
+        <div className="bg-green-200 text-green-800 p-2 mb-4 rounded">
           {alertMessage}
         </div>
       )}
-      <form className="mt-6 space-y-4">
+      <form className="space-y-4">
         <div className="mb-4">
           <label htmlFor="title" className="block text-lg font-semibold">
             Topic:
@@ -85,19 +85,31 @@ function Contact() {
             name="description"
             onChange={handleMessageChange}
             value={newEmail.description}
-            placeholder="Try your message here"
+            placeholder="Type your message here"
             rows={6}
             required
           />
         </div>
         <button
-          className="bg-black text-white py-2 px-4 rounded-md w-full text-lg font-semibold"
+          className="bg-black text-white py-2 px-4 rounded-md w-full text-lg font-semibold hover:bg-gray-800 hover:text-gray-100 transition-all duration-300"
           type="button"
           onClick={handleSubmit}
         >
           Submit
         </button>
       </form>
+      <div className="mt-8">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3192.06300832411!2d174.7764339931746!3d-36.8649106243409!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d0d47c43b7c00a3%3A0x4abe94b659ab7fb0!2sDev%20Academy%20Aotearoa%20-%20Auckland%20Campus!5e0!3m2!1sen!2snz!4v1695091841249!5m2!1sen!2snz"
+          title="map"
+          width="830"
+          height="450"
+          style={{ border: 0 }}
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+        <p>(this address is not relevant to this website)</p>
+      </div>
     </div>
   )
 }
