2cacccbf2a98cef84baffd928dfad5891cf8de1d
Rigelle Cruz <rigellecruz@gmail.com>
2023-09-19 12:23:31 +1200

    add background elements to homepage

--- client/user/pages/Home/Home.tsx
+++ client/user/pages/Home/Home.tsx
@@ -6,38 +6,46 @@ const Home = () => {
     navigate(link)
   }
   return (
-    <div
-      className=" flex items-center justify-center"
-      style={{ background: '#FFC5C7' }}
-    >
-      <div className="max-w-screen-xl flex flex-col items-center p-2">
-        {/* Welcome Card */}
-        <div className=" p-10 space-y-12 flex flex-row items-center w-4/5">
-          <img
-            src="/images/home-tea.svg"
-            alt="Boba Buddies Logo"
-            className="max-w-full h-auto pr-10"
-            style={{ width: '65%', maxWidth: '100%' }}
-          />
+    <div style={{ background: '#FFC5C7' }}>
+      <div
+        className="bg-red-200 min-h-screen flex items-center justify-center"
+        style={{
+          backgroundImage: `url('/images/ellipse1.svg'), url('/images/ellipse2.svg')`,
+          backgroundPosition: 'bottom left, top right',
+          backgroundRepeat: 'no-repeat',
+        }}
+      >
+        <div className=" flex items-center justify-center">
+          <div className="max-w-screen-xl flex flex-col items-center p-2">
+            {/* Welcome Card */}
+            <div className=" p-10 space-y-12 flex flex-row items-center w-4/5">
+              <img
+                src="/images/home-tea.svg"
+                alt="Boba Buddies Logo"
+                className="max-w-full h-auto pr-10"
+                style={{ width: '65%', maxWidth: '100%' }}
+              />
 
-          <div className=" flex flex-col text-white text-xl space-y-8 p-10">
-            <img
-              src="/images/home-title.svg"
-              alt="Boba Buddies Logo"
-              className="max-w-full h-auto"
-              style={{ width: '90%', maxWidth: '100%' }}
-            />
-            <p>
-              Dive into our colorful world of delicious flavors, hand-shaken to
-              perfection. Join the fun, and become a boba buddy today!
-            </p>
+              <div className=" flex flex-col text-white text-xl space-y-8 p-10">
+                <img
+                  src="/images/home-title.svg"
+                  alt="Boba Buddies Logo"
+                  className="max-w-full h-auto"
+                  style={{ width: '90%', maxWidth: '100%' }}
+                />
+                <p>
+                  Dive into our colorful world of delicious flavors, hand-shaken
+                  to perfection. Join the fun, and become a boba buddy today!
+                </p>
 
-            <button
-              onClick={() => goTo('shop')}
-              className="w-2/5 border border-white border-2  text-white px-6 py-2 rounded-2xl hover:bg-white hover:text-rose-300 transition-all"
-            >
-              Shop
-            </button>
+                <button
+                  onClick={() => goTo('shop')}
+                  className="w-2/5 border border-white border-2  text-white px-6 py-2 rounded-2xl hover:bg-white hover:text-rose-300 transition-all"
+                >
+                  Shop
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
