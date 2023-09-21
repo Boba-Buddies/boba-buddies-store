a137f46d6e16edd7604cc0cd02565856eeb6e97f
Jane Zhang <jiejanezhang2012@gmail.com>
2023-09-21 13:22:17 +1200

    working on some issues

--- client/admin/pages/IndividualProduct/IndividualProduct.tsx
+++ client/admin/pages/IndividualProduct/IndividualProduct.tsx
@@ -1,19 +1,17 @@
 import { useQuery } from 'react-query'
 import { useParams } from 'react-router-dom'
 import { fetchProductByIdAdmin } from '../../../apis/products'
-import { fetchReviewsByProductId } from '../../../apis/reviews'
-import { ProductReview } from '../../../../models/Reviews'
-import { fetchWishlistStatusByProductId } from '../../../apis/wishlist'
 import { useAuth0 } from '@auth0/auth0-react'
 import LoadError from '../../../user/components/LoadError/LoadError'
 import EditProduct from '../../components/EditProduct/EditProduct'
+import { useEffect } from 'react'
 
 const IndividualProduct = () => {
   const { getAccessTokenSilently } = useAuth0() // Use Auth0 hook
   const params = useParams()
   const id = Number(params.id)
 
-  const { data: product, status: statusProductS } = useQuery(
+  const { data: product, status: statusProduct, isLoading } = useQuery(
     ['getProduct', id],
     async () => {
       const token = await getAccessTokenSilently()
@@ -21,41 +19,17 @@ const IndividualProduct = () => {
     },
   )
 
-  const {
-    data: reviews,
-    refetch: refetchReviews,
-    status: statusReviews,
-  } = useQuery(['getReviews', id], async () => {
-    const token = await getAccessTokenSilently()
-    const fetchedReviews: ProductReview[] = await fetchReviewsByProductId(
-      id,
-      token,
-    )
-    return fetchedReviews
-  })
 
-  const {
-    data: wishlistStatus = false,
-    refetch: refetchWishlistProductStatus,
-    status: statusWishlist,
-  } = useQuery(['getWishlistStatus', id], async () => {
-    const token = await getAccessTokenSilently()
-    const wishlistStatus: boolean = await fetchWishlistStatusByProductId(
-      id,
-      token,
-    )
-    return wishlistStatus
-  })
 
   return (
     <>
-      <LoadError status={[statusProductS, statusReviews, statusWishlist]} />
-      {product && reviews && (
+      <LoadError status={[statusProduct]} />
+      {!isLoading && product && (
         <div
           className="flex flex-col items-center w-full"
           style={{ marginTop: '100px', marginBottom: '150px' }}
         >
-          <EditProduct
+          <EditProduct key={product.id}
             product={product}
           />
         </div>
