96234fe80015e9f65e59973695b9d27c781d810a
Rigelle Cruz <rigellecruz@gmail.com>
2023-09-21 18:21:09 +1200

    make progress on unprotected component

--- client/UI/UnprotectedComponent.tsx
+++ client/UI/UnprotectedComponent.tsx
@@ -1,20 +1,10 @@
 import React from 'react'
-import { useAuth0 } from '@auth0/auth0-react'
 
 interface Props {
   component: React.ComponentType<unknown>
 }
 
-export const UnprotectedComponent = ({ component }: Props) => {
-  //const { isLoading } = useAuth0()
-
-  // if (isLoading) {
-  //   // Render loading indicator or placeholder content while Auth0 is being initialized
-  //   return
-  // }
-
-  // Render the component if the user is authenticated
-  const Component = component
+export const UnprotectedComponent = ({ component: Component }: Props) => {
   return <Component />
 }
 
