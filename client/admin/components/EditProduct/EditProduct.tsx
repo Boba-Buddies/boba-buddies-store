0a00ac30533ce125e951ad2811e32b2859fb93e7
Jane Zhang <jiejanezhang2012@gmail.com>
2023-09-21 15:55:32 +1200

    stylying

--- client/admin/components/EditProduct/EditProduct.tsx
+++ client/admin/components/EditProduct/EditProduct.tsx
@@ -79,15 +79,16 @@ function EditProduct({ product, }: EditProductProps) {
 
   return (
     <form>
-      <div className="flex flex-col max-w-5xl" style={{ padding: '10px' }}>
-        <div className="flex flex-row items-center mb-16">
-          <img src={product.image} alt={product.name} className="w-1/4" />
+      <div className="flex flex-col max-w-5xl mx-auto p-4">
+        <div className="mt-4 flex flex-row justify-between flex-wrap">
 
-          <div className="w-1/2 flex flex-col justify-center">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="imageUrl"
-            >
+          <div className="lg:w-2/4">
+            <img src={product.image} alt={product.name} className="w-full" />
+          </div>
+
+          <div className="lg:w-2/4 p-4 self-end">
+
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="imageUrl">
               Image URL:
             </label>
             <input
@@ -100,21 +101,21 @@ function EditProduct({ product, }: EditProductProps) {
             />
           </div>
         </div>
-        <div className='w-4/5'>
-          <div className='flex flex-row justify-between'>
-            <div className='ml-6'>
-              <button
-                className={`font-bold text-white py-2 px-4 rounded ${editedProduct.isEnabled ? 'bg-green-500' : 'bg-red-500'
-                  }`}
-                type="button"
-                onClick={toggleEnabled}
-              >
-                {editedProduct.isEnabled ? 'Enabled' : 'Disabled'}
-              </button>
-            </div>
+        <div className="p-4">
+
+          <div className="mt-4 flex flex-row justify-between">
+            <button
+              className={`font-bold mt-4 lg:mt-0 lg:ml-4 text-white py-2 px-4 rounded ${editedProduct.isEnabled ? 'bg-green-500' : 'bg-red-500'
+                }`}
+              type="button"
+              onClick={toggleEnabled}
+            >
+              {editedProduct.isEnabled ? 'Enabled' : 'Disabled'}
+            </button>
 
             <button
-              className="font-bold py-2 px-4 rounded  bg-black text-white transition-colors hover:bg-sky-600 hover:text-black"
+              className="font-bold py-2 px-4 rounded bg-black text-white transition-colors hover:bg-sky-600 hover:text-black 
+              mt-4 lg:mt-0 lg:ml-4"
               type="button"
               onClick={saveChanges}
             >
@@ -122,33 +123,63 @@ function EditProduct({ product, }: EditProductProps) {
             </button>
           </div>
 
-
-          <div className='mt-6 ml-6'>
-            <input className="text-3xl font-bold" onChange={handleChange} name='name' type='text' value={editedProduct.name}></input>
+          <div className="mt-4">
+            <input
+              className="text-3xl font-bold w-full"
+              onChange={handleChange}
+              name="name"
+              type="text"
+              value={editedProduct.name}
+            />
           </div>
 
-          <div className='flex flex-row justify-between mt-6 ml-6'>
-            <div className=" flex items-center text-3xl font-bold mr-6">
-              <label className='w-max' htmlFor="price">Price: $</label>
-              <input className='border ml-1' onChange={handleChange} name='price' type='number' value={editedProduct.price}></input>
+          <div className="mt-4 flex flex-row justify-between">
+
+            <div className="flex flex-col lg:flex-row justify-between mt-4">
+              <div className="lg:flex items-center text-sm font-bold">
+                <label className="w-max" htmlFor="price">
+                  Price: $
+                </label>
+                <input
+                  className="border ml-1 w-full lg:w-1/2"
+                  onChange={handleChange}
+                  name="price"
+                  type="number"
+                  value={editedProduct.price}
+                />
+              </div>
+
+              <div className="lg:flex items-center text-sm font-bold mt-4 lg:mt-0">
+                <label htmlFor="stock"> Stock: </label>
+                <input
+                  className="border ml-1 w-full lg:w-1/2"
+                  onChange={handleChange}
+                  name="stock"
+                  type="number"
+                  value={editedProduct.stock}
+                />
+              </div>
             </div>
-
-            <div className="flex items-center text-3xl font-bold">
-              <label htmlFor="stock"> Stock: </label>
-              <input className='border ml-1' onChange={handleChange} name='stock' type='number' value={editedProduct.stock}></input>
-            </div>
           </div>
 
-          <div className='mt-6'>
-            <label className='text-3xl font-bold ml-6' htmlFor="description">Description</label>
-            <textarea rows={10} cols={100} className="border text-xl ml-6 w-max" onChange={handleChange} name='description' value={editedProduct.description}></textarea>
+          <div className="mt-4 text-sm">
+            <label className="font-bold" htmlFor="description">
+              Description
+            </label>
+            <textarea
+              rows={10}
+              cols={100}
+              className="border w-full mt-2"
+              onChange={handleChange}
+              name="description"
+              value={editedProduct.description}
+            ></textarea>
           </div>
-
         </div>
-
       </div>
     </form>
-  )
+  );
 }
+
 
 export default EditProduct
