d2fed4affdc188f7847d147224947da843f072ea
Sarah Chung <sarahchungwu@gmail.com>
2023-09-26 12:04:35 +1300

    finished the db function test for getProductByIdAdmin

--- server/db/products.test.ts
+++ server/db/products.test.ts
@@ -25,15 +25,30 @@ describe('getAllProductsAdmin', () => {
 
   it('returns all the products which contian isEnable is true or false', async () => {
     const isEnable = 1
-    const AdminProduct = await db.getAllProductsAdmin(testDb)
-    expect(AdminProduct[0].isEnabled).toBe(isEnable)
+    const adminProduct = await db.getAllProductsAdmin(testDb)
+    expect(adminProduct[0].isEnabled).toBe(isEnable)
   })
 })
 
 describe('getAllProductsUser', () => {
   it('returns all the products for user page', async () => {
     const testProductName = 'Pearl Milk Tea'
-    const AdminProduct = await db.getAllProductsAdmin(testDb)
-    expect(AdminProduct[0].name).toBe(testProductName)
+    const adminProduct = await db.getAllProductsAdmin(testDb)
+    expect(adminProduct[0].name).toBe(testProductName)
+  })
+})
+
+describe('getProductByIdAdmin', () => {
+  it('returns the products by productId for admin page', async () => {
+    const testProductId = 5
+    const testProductName = 'Brown Sugar Milk Tea and Pearls'
+    const adminProducts = await db.getProductByIdAdmin(testProductId, testDb)
+    expect(adminProducts.name).toBe(testProductName)
+  })
+
+  it('returns an empty object if there is no matched productId', async () => {
+    const testProductId = 1123
+    const adminProducts = await db.getProductByIdAdmin(testProductId, testDb)
+    expect(adminProducts).toBeUndefined()
   })
 })
