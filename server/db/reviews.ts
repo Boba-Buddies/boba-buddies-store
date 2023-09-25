abe89bf017dc34230a2aa05c5d0d2e892c600045
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-09-26 11:01:09 +1300

    pass database from addReviewByUserId into recalculateAverageRatingByProductId

--- server/db/reviews.ts
+++ server/db/reviews.ts
@@ -128,7 +128,7 @@ export async function addReviewByUserId(newReview: NewReview, userId: string, db
   })
 
   //After we add the review, we recalcaute the average_rating of the associated product, taking into account the new review's rating that was just added.
-  await recalculateAverageRatingByProductId(newReview.productId)
+  await recalculateAverageRatingByProductId(newReview.productId, db)
 }
 
 export async function updateReviewStatusById(
