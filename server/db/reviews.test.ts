946aeb189188720e44c5cc94b5c0a9f01e138f32
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-09-25 15:05:04 +1300

    create test for getReviewById

--- server/db/reviews.test.ts
+++ server/db/reviews.test.ts
@@ -1,4 +1,12 @@
-import { beforeEach, beforeAll, afterAll, describe, it, expect } from 'vitest'
+import {
+  beforeEach,
+  beforeAll,
+  afterAll,
+  describe,
+  it,
+  expect,
+  vi,
+} from 'vitest'
 import knex from 'knex'
 
 import * as db from './reviews'
@@ -17,15 +25,27 @@ afterAll(async () => {
   await testDb.destroy()
 })
 
-//getReviewsByProductId
-
+//getReviewsByProductId - FLAKY!
 describe('getReviewsByProductId', () => {
-  it('returns reviews that match product Id', async () => {
+  it('returns reviews that match given product id', async () => {
     const testProductId = 1
     const reviews = await db.getReviewsByProductId(testProductId, testDb)
-
     expect(reviews[0].productId).toBe(testProductId)
   })
 })
 
+//!getAmountOfReviewsByDate
 
+//!getAllReviews
+
+//getReviewById
+describe('getReviewById', () => {
+  it('returns review that matches given id', async () => {
+    const testId = 1
+    const review = await db.getReviewById(testId, testDb)
+    expect(review.reviewId).toBe(testId)
+  })
+})
+
+//!recalculateAverageRatingByProductId
+//
