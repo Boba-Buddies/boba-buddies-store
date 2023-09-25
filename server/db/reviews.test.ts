c04eac89b9a6c9f27ec8661aeb2b61eacdb4a7b9
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-09-25 14:08:40 +1300

    convert jest test environment to vitest in reviews

--- server/db/reviews.test.ts
+++ server/db/reviews.test.ts
@@ -1,62 +1,20 @@
-import * as reviews from './reviews'
-import Knex from 'knex';
+import { beforeEach, beforeAll, afterAll, describe, it, expect } from 'vitest'
+import knex from 'knex'
 
-jest.mock('knex');
+import * as db from './reviews'
+import config from './knexfile'
+const testDb = knex(config.test)
 
-const mockDb: any = {
-  from: jest.fn().mockReturnThis(),
-  join: jest.fn().mockReturnThis(),
-  where: jest.fn().mockReturnThis(),
-  whereRaw: jest.fn().mockReturnThis(),
-  select: jest.fn().mockReturnThis(),
-  count: jest.fn().mockReturnThis(),
-  first: jest.fn().mockReturnThis(),
-  mockReturnThis: jest.fn().mockReturnThis(),
-}
-
-jest.mock('./connection', () => {
-  return jest.fn(() => mockDb)
+beforeAll(async () => {
+  await testDb.migrate.latest()
 })
 
-describe('Reviews tests', () => {
-  beforeEach(() => {
-    jest.clearAllMocks()
-  })
-
-  it('getReviewsByProductId', async () => {
-    mockDb.select.mockResolvedValueOnce([
-      {
-        productId: 1,
-        userName: 'John',
-        rating: 4.5,
-        createdAt: '2023-08-08',
-        description: 'Great product',
-      },
-    ])
-
-    const result = await reviews.getReviewsByProductId(1)
-    expect(result).toEqual([
-      {
-        productId: 1,
-        userName: 'John',
-        rating: 4.5,
-        createdAt: '2023-08-08',
-        description: 'Great product',
-      },
-    ])
-  })
-
-  it('getAmountOfReviewsByDate', async () => {
-    const mockReviewsData = [
-      { created_at: '2023-08-08T14:30:00' },
-      { created_at: '2023-08-08T09:15:00' },
-      { created_at: '2023-08-08T19:15:00' },
-      { created_at: '2023-08-09T14:30:00' },
-    ]
-
-    mockDb.mockResolvedValueOnce(mockReviewsData)
-
-    const result = await reviews.getAmountOfReviewsByDate('2023-08-08')
-    expect(result).toEqual({ reviewCount: 3 })
-  })
+beforeEach(async () => {
+  await testDb.seed.run()
 })
+
+afterAll(async () => {
+  await testDb.destroy()
+})
+
+//getReviewsByProductId
