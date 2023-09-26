39c53b808f2d7912f64223ef250768dba678d955
Sarah Chung <sarahchungwu@gmail.com>
2023-09-27 10:56:17 +1300

    finished the test for post and patch request test for the products

--- server/routes/products.test.ts
+++ server/routes/products.test.ts
@@ -4,7 +4,12 @@ import server from '../server'
 import * as db from '../db/products'
 import { isUserAdmin } from '../db/users'
 import { getMockToken } from './mockToken'
-import { AdminProduct, UserProduct } from '../../models/Products'
+import {
+  AdminProduct,
+  LowStockProducts,
+  UpsertProduct,
+  UserProduct,
+} from '../../models/Products'
 
 vi.mock('../db/products.ts')
 vi.mock('../logger.ts')
@@ -153,7 +158,7 @@ describe('GET /api/v1/products/admin/:id', () => {
     }
 
     vi.mocked(db.getProductByIdAdmin).mockResolvedValue(fakeAdminProduct)
-    vi.mocked(isUserAdmin).mockResolvedValue(true)
+
     const response = await request(server)
       .get(`/api/v1/products/admin/${fakeId}`)
       .set('authorization', `Bearer ${getMockToken()}`)
@@ -181,6 +186,224 @@ describe('GET /api/v1/products/admin/:id', () => {
     const response = await request(server)
       .get(`/api/v1/products/admin/${fakeId}`)
       .set('authorization', `Bearer ${getMockToken()}`)
+    expect(response.status).toBe(401)
+    expect(response.body).toEqual({
+      message: 'user is not authorized as admin',
+    })
+  })
+})
+
+describe('GET /api/v1/products/lowstock/:maxStock', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+  it('should return 200 and low Stock products for an admin user', async () => {
+    vi.mocked(isUserAdmin).mockResolvedValue(true)
+    const fakeMaxStock = 2
+    const fakeLowStockProducts: LowStockProducts[] = [
+      {
+        id: 123,
+        name: 'Apple',
+        image: 'Apple.jpg',
+      },
+    ]
+
+    vi.mocked(db.getAmountOfProductsBelowStockLevel).mockResolvedValue(
+      fakeLowStockProducts,
+    )
+
+    const response = await request(server)
+      .get(`/api/v1/products/lowstock/${fakeMaxStock}`)
+      .set('authorization', `Bearer ${getMockToken()}`)
+    expect(response.status).toBe(200)
+    expect(response.body.lowStockProducts).toEqual(fakeLowStockProducts)
+  })
+
+  it('should return 500 when no access token is passed', async () => {
+    vi.mocked(isUserAdmin).mockResolvedValue(true)
+    vi.mocked(db.getProductByIdAdmin).mockRejectedValue(new Error('test'))
+
+    const fakeMaxStock = 2
+    const response = await request(server)
+      .get(`/api/v1/products/lowstock/${fakeMaxStock}`)
+      .set('authorization', `Bearer ${getMockToken()}`)
+    expect(response.status).toBe(500)
+    expect(response.body).toEqual({
+      message: 'Unable to get the data from database',
+    })
+  })
+  it('should return 401 for a non-admin user', async () => {
+    vi.mocked(db.getProductByIdAdmin).mockRejectedValue(new Error('test'))
+    const fakeMaxStock = 2
+    vi.mocked(isUserAdmin).mockResolvedValue(false)
+    const response = await request(server)
+      .get(`/api/v1/products/lowstock/${fakeMaxStock}`)
+      .set('authorization', `Bearer ${getMockToken()}`)
+    expect(response.status).toBe(401)
+    expect(response.body).toEqual({
+      message: 'user is not authorized as admin',
+    })
+  })
+})
+
+describe('POST /api/v1/products', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+  it('should return 201 when creating a new product', async () => {
+    vi.mocked(isUserAdmin).mockResolvedValue(true)
+    const fakeProduct: UpsertProduct = {
+      name: 'Apple',
+      image: 'Apple.jpg',
+      price: 99,
+      description: 'yummy Apple',
+      stock: 10,
+      isEnabled: true,
+    }
+
+    vi.mocked(db.addProduct).mockResolvedValue([1])
+    const response = await request(server)
+      .post('/api/v1/products')
+      .set('authorization', `Bearer ${getMockToken()}`)
+      .send(fakeProduct)
+    expect(response.status).toBe(201)
+  })
+
+  it('should return 400 if the body does not match the zod schemea', async () => {
+    vi.mocked(isUserAdmin).mockResolvedValue(true)
+    const fakeProduct = {}
+
+    vi.mocked(db.addProduct).mockResolvedValue([1])
+    const response = await request(server)
+      .post('/api/v1/products')
+      .set('authorization', `Bearer ${getMockToken()}`)
+      .send(fakeProduct)
+    expect(response.status).toBe(400)
+  })
+
+  it('should return 500 when no access token is passed', async () => {
+    vi.mocked(isUserAdmin).mockResolvedValue(true)
+    const fakeProduct: UpsertProduct = {
+      name: 'Apple',
+      image: 'Apple.jpg',
+      price: 99,
+      description: 'yummy Apple',
+      stock: 10,
+      isEnabled: true,
+    }
+
+    vi.mocked(db.addProduct).mockRejectedValue(new Error('test'))
+
+    const response = await request(server)
+      .post('/api/v1/products')
+      .set('authorization', `Bearer ${getMockToken()}`)
+      .send(fakeProduct)
+    expect(response.status).toBe(500)
+    expect(response.body).toEqual({
+      message: 'Unable to insert new Product to database',
+    })
+  })
+
+  it('should return 401 for a non-admin user', async () => {
+    vi.mocked(db.addProduct).mockRejectedValue(new Error('test'))
+    vi.mocked(isUserAdmin).mockResolvedValue(false)
+
+    const fakeProduct: UpsertProduct = {
+      name: 'Apple',
+      image: 'Apple.jpg',
+      price: 99,
+      description: 'yummy Apple',
+      stock: 10,
+      isEnabled: true,
+    }
+    const response = await request(server)
+      .post('/api/v1/products')
+      .set('authorization', `Bearer ${getMockToken()}`)
+      .send(fakeProduct)
+    expect(response.status).toBe(401)
+    expect(response.body).toEqual({
+      message: 'user is not authorized as admin',
+    })
+  })
+})
+
+describe('PATCH /api/v1/products/:id', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+  it('should return 201 when creating a new product', async () => {
+    vi.mocked(isUserAdmin).mockResolvedValue(true)
+    const fakeProdutId = 123
+    const fakeProduct: UpsertProduct = {
+      name: 'Apple',
+      image: 'Apple.jpg',
+      price: 99,
+      description: 'yummy Apple',
+      stock: 10,
+      isEnabled: true,
+    }
+
+    vi.mocked(db.updateProduct).mockResolvedValue(1)
+    const response = await request(server)
+      .patch(`/api/v1/products/${fakeProdutId}`)
+      .set('authorization', `Bearer ${getMockToken()}`)
+      .send(fakeProduct)
+    expect(response.status).toBe(201)
+  })
+
+  it('should return 400 if the body does not match the zod schemea', async () => {
+    vi.mocked(isUserAdmin).mockResolvedValue(true)
+    const fakeProdutId = 123
+    const fakeProduct = {}
+
+    vi.mocked(db.updateProduct).mockResolvedValue(1)
+    const response = await request(server)
+      .patch(`/api/v1/products/${fakeProdutId}`)
+      .set('authorization', `Bearer ${getMockToken()}`)
+      .send(fakeProduct)
+    expect(response.status).toBe(400)
+  })
+
+  it('should return 500 when no access token is passed', async () => {
+    vi.mocked(isUserAdmin).mockResolvedValue(true)
+    const fakeProdutId = 123
+    const fakeProduct: UpsertProduct = {
+      name: 'Apple',
+      image: 'Apple.jpg',
+      price: 99,
+      description: 'yummy Apple',
+      stock: 10,
+      isEnabled: true,
+    }
+
+    vi.mocked(db.updateProduct).mockRejectedValue(new Error('test'))
+
+    const response = await request(server)
+      .patch(`/api/v1/products/${fakeProdutId}`)
+      .set('authorization', `Bearer ${getMockToken()}`)
+      .send(fakeProduct)
+    expect(response.status).toBe(500)
+    expect(response.body).toEqual({
+      message: 'Unable to insert new Product to database',
+    })
+  })
+
+  it('should return 401 for a non-admin user', async () => {
+    vi.mocked(db.updateProduct).mockRejectedValue(new Error('test'))
+    vi.mocked(isUserAdmin).mockResolvedValue(false)
+    const fakeProdutId = 123
+    const fakeProduct: UpsertProduct = {
+      name: 'Apple',
+      image: 'Apple.jpg',
+      price: 99,
+      description: 'yummy Apple',
+      stock: 10,
+      isEnabled: true,
+    }
+    const response = await request(server)
+      .patch(`/api/v1/products/${fakeProdutId}`)
+      .set('authorization', `Bearer ${getMockToken()}`)
+      .send(fakeProduct)
     expect(response.status).toBe(401)
     expect(response.body).toEqual({
       message: 'user is not authorized as admin',
