2e61a977fb8514d6eb40d7e9614e599cabcfbc06
Sarah Chung <sarahchungwu@gmail.com>
2023-09-26 15:54:51 +1300

    created the test for  get route /api/v1/products/admin

--- server/routes/products.test.ts
+++ server/routes/products.test.ts
@@ -2,11 +2,13 @@ import { vi, describe, it, expect, afterEach } from 'vitest'
 import request from 'supertest'
 import server from '../server'
 import * as db from '../db/products'
+import { isUserAdmin } from '../db/users'
 import { getMockToken } from './mockToken'
-import { UserProduct } from '../../models/Products'
+import { AdminProduct, UserProduct } from '../../models/Products'
 
 vi.mock('../db/products.ts')
 vi.mock('../logger.ts')
+vi.mock('../db/users.ts')
 
 //test for the users
 describe('GET /api/v1/products', () => {
@@ -70,6 +72,47 @@ describe('GET /api/v1/products/:id', () => {
     const productId = 123
     vi.mocked(db.getProductByIdUser).mockRejectedValue(new Error('test'))
     const response = await request(server).get(`/api/v1/products/${productId}`)
+    expect(response.status).toBe(500)
+    expect(response.body).toEqual({
+      message: 'Unable to get the data from database',
+    })
+  })
+})
+
+//For admin
+
+describe('GET /api/v1/products/admin', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+  it('should return 200 and all products for an admin user', async () => {
+    vi.mocked(isUserAdmin).mockResolvedValue(true)
+    const fakeAdminProducts: AdminProduct[] = [
+      {
+        id: 123,
+        name: 'Banana Tea',
+        image: '/images/banana.svg',
+        price: 100,
+        description: 'banana is good.',
+        stock: 200,
+        averageRating: 3.75,
+        isEnabled: true,
+      },
+    ]
+
+    vi.mocked(db.getAllProductsAdmin).mockResolvedValue(fakeAdminProducts)
+    const response = await request(server)
+      .get('/api/v1/products/admin')
+      .set('authorization', `Bearer ${getMockToken()}`)
+    expect(response.status).toBe(200)
+    expect(response.body).toEqual(fakeAdminProducts)
+  })
+
+  it('should return 500 when no access token is passed', async () => {
+    vi.mocked(db.getAllProductsAdmin).mockRejectedValue(new Error('test'))
+    const response = await request(server)
+      .get('/api/v1/shipping-options')
+      .set('authorization', `Bearer ${getMockToken()}`)
     expect(response.status).toBe(500)
     expect(response.body).toEqual({
       message: 'Unable to get the data from database',
