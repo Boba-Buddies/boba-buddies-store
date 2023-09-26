4d0124dfc55b087e97b04747466a08ac49bea265
Sarah Chung <sarahchungwu@gmail.com>
2023-09-26 15:02:05 +1300

    created the test for get /api/v1/products/:id

--- server/routes/products.test.ts
+++ server/routes/products.test.ts
@@ -42,3 +42,37 @@ describe('GET /api/v1/products', () => {
     })
   })
 })
+
+describe('GET /api/v1/products/:id', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+  it('should return the product by id for the user page', async () => {
+    const productId = 123
+    const fakeProduct: UserProduct = {
+      id: 123,
+      name: 'Banana Tea',
+      image: '/images/banana.svg',
+      price: 100,
+      description: 'banana is good.',
+      stock: 200,
+      averageRating: 3.75,
+    }
+
+    vi.mocked(db.getProductByIdUser).mockResolvedValue(fakeProduct)
+    const response = await request(server).get(`/api/v1/products/${productId}`)
+
+    expect(response.status).toBe(200)
+    expect(response.body).toEqual(fakeProduct)
+  })
+
+  it('should return 500 when unable to get the data from database', async () => {
+    const productId = 123
+    vi.mocked(db.getProductByIdUser).mockRejectedValue(new Error('test'))
+    const response = await request(server).get(`/api/v1/products/${productId}`)
+    expect(response.status).toBe(500)
+    expect(response.body).toEqual({
+      message: 'Unable to get the data from database',
+    })
+  })
+})
