e15b13a511c1e3d72bfd2387536d9c4e401c8fd8
Jane Zhang <jiejanezhang2012@gmail.com>
2023-10-02 13:15:51 +1300

    completed test for addToWishlistByProductId

--- server/db/wishlist.test.ts
+++ server/db/wishlist.test.ts
@@ -9,6 +9,7 @@ import {
 import * as db from './wishlist'
 import knex from 'knex'
 import config from './knexfile'
+import { error } from 'console'
 const testDb = knex(config.test)
 
 beforeAll(async () => {
@@ -59,5 +60,31 @@ describe('getWishlistStatusByProductId', () => {
     const testUserId = 'auth0|abc12345'
     const result = await db.getWishlistStatusByProductId(testProductId, testUserId, testDb)
     expect(result).toBeFalsy
+  })
+})
+
+describe('addToWishlistByProductId', () => {
+  it('product added to wishlist successfully', async () => {
+    const testProductId = 12
+    const testUserId = 'auth0|abc12345'
+    await db.addToWishlistByProductId(testProductId, testUserId, testDb)
+
+    const result = await db.getWishlistStatusByProductId(testProductId, testUserId, testDb)
+    expect(result).toBeTruthy
+  })
+
+  it('return error message with exsiting wishlist product', async () => {
+    const testProductId = 1
+    const testUserId = 'auth0|abc12345'
+    // Use a try-catch block to catch the error thrown by addToWishlistByProductId
+    try {
+      await db.addToWishlistByProductId(testProductId, testUserId, testDb)
+      // If the above line doesn't throw an error, fail the test
+      fail('expected an error to be thrown')
+    } catch (error) {
+      // Check if the error message matches the expected message
+      expect(error.message).toBe('Item already in wishlist')
+    }
+
   })
 })
