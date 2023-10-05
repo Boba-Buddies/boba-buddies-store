e18f3ae86b4476b34a983794cc88bb675ce96e39
Jane Zhang <jiejanezhang2012@gmail.com>
2023-10-05 15:16:37 +1300

    completed test for wishlist routes

--- server/routes/wishlist.test.ts
+++ server/routes/wishlist.test.ts
@@ -2,7 +2,6 @@ import { vi, describe, it, expect, afterEach } from 'vitest'
 import request from 'supertest'
 import server from '../server'
 import * as db from '../db/wishlist'
-import { isUserAdmin } from '../db/users'
 import { getMockToken } from './mockToken'
 import { WishlistProduct } from '../../models/Wishlist'
 
@@ -14,7 +13,7 @@ describe('GET /api/v1/wishlist', () => {
   afterEach(() => {
     vi.resetAllMocks()
   })
-  it('should return 200 with a user', async () => {
+  it('should return 200/ should retrieve the user\'s wishlist', async () => {
     const fakeWishlist: WishlistProduct[] = [
       {
         id: 1,
@@ -33,6 +32,13 @@ describe('GET /api/v1/wishlist', () => {
 
     expect(response.status).toBe(200)
     expect(response.body).toEqual(fakeWishlist)
+  })
+
+  it('should handle authentication failure (no token)', async () => {
+    const response = await request(server)
+      .get('/api/v1/wishlist')
+
+    expect(response.status).toBe(401)
   })
 
   it('should return 500 when no access token is passed', async () => {
@@ -62,14 +68,66 @@ describe('GET /api/v1/wishlist/status/:productId', () => {
   })
 
   it('should return 500 when no access token is passed', async () => {
-    const fakeProdutId = 1
+    const fakeProductId = 1
     vi.mocked(db.getWishlistStatusByProductId).mockRejectedValue(new Error('test'))
     const response = await request(server)
-      .get(`/api/v1/wishlist/status/${fakeProdutId}`)
+      .get(`/api/v1/wishlist/status/${fakeProductId}`)
       .set('authorization', `Bearer ${getMockToken()}`)
     expect(response.status).toBe(500)
     expect(response.body).toEqual({
       message: 'Unable to get the wishlist status',
     })
+  })
+})
+
+describe('POST /api/v1/wishlist', () => {
+  afterEach(() => { vi.resetAllMocks() })
+  it('should return 201 when a product is added successfully to the wishlist', async () => {
+    const fakeWishlist = {
+      id: 1,
+      user_id: 'auth0|abc12345',
+      productId: 1,
+    }
+    vi.mocked(db.addToWishlistByProductId).mockResolvedValue([1])
+    const response = await request(server)
+      .post('/api/v1/wishlist')
+      .set('authorization', `Bearer ${getMockToken()}`)
+      .send(fakeWishlist)
+    expect(response.status).toBe(201)
+  })
+
+  it('should return 500 when no access token is passed', async () => {
+    const fakeWishlist = {
+      id: 1,
+      user_id: 'auth0|abc12345',
+      productId: 1,
+    }
+    vi.mocked(db.addToWishlistByProductId).mockRejectedValue(new Error('test'))
+    const response = await request(server)
+      .post('/api/v1/wishlist')
+      .set('authorization', `Bearer ${getMockToken()}`)
+      .send(fakeWishlist)
+    expect(response.status).toBe(500)
+    expect(response.body).toEqual({ message: 'Unable to insert new Product to database' })
+  })
+})
+
+describe('DELETE /api/v1/wishlist/:id', () => {
+  it('should return 200 when successfully deleted', async () => {
+    const fakeProductId = 1
+    vi.mocked(db.removeFromWishlistByProductId)
+    const response = await request(server)
+      .del(`/api/v1/wishlist/${fakeProductId}`)
+      .set('authorization', `Bearer ${getMockToken()}`)
+    expect(response.status).toBe(200)
+  })
+
+  it('should return 500 when no access token is passed', async () => {
+    const fakeProductId = 1
+    vi.mocked(db.removeFromWishlistByProductId).mockRejectedValue(new Error('test'))
+    const response = await request(server)
+      .del(`/api/v1/wishlist/${fakeProductId}`)
+      .set('authorization', `Bearer ${getMockToken()}`)
+    expect(response.status).toBe(500)
   })
 })
